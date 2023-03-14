f12b52351d9d9b3909e6da26e0975c89e3828fa4
Sperling-0 <tanuja.doriya@wikimedia.de>
2023-03-14 09:52:42 +0100

    Fix banner dashboard
    - Banner compiles successfully
    - Compiled banner gets copied successfully

--- dashboard/components/BannerActions.vue
+++ dashboard/components/BannerActions.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue';
+import { computed, Ref, ref } from 'vue';
 import type { Banner, Campaign } from '../../webpack/campaign_config_types';
 import IconPreview from './IconPreview.vue';
 import IconEdit from './IconEdit.vue';
@@ -58,7 +58,7 @@ const props = defineProps<{
 	banner: Banner,
 	campaign: Campaign,
 	isWPDE: boolean,
-	compileInfo: CompileInfo
+	compileInfo?: CompileInfo
 }>();
 
 const bannerPageName = ref( props.banner.pageName );
@@ -66,11 +66,26 @@ const bannerPageName = ref( props.banner.pageName );
 const CENTRAL_NOTICE_EDIT_URL = 'https://meta.wikimedia.org/wiki/Special:CentralNoticeBanners/edit/{{banner}}';
 const WPDE_GITHUB_REPO = 'https://github.com/wmde/wikipedia.de-banners/blob/master/campaigns.yml';
 
-let editLink: string = props.isWPDE ? WPDE_GITHUB_REPO : CENTRAL_NOTICE_EDIT_URL.replace( '{{banner}}', bannerPageName.value );
-const isCompiled: boolean = !!props.compileInfo;
+const editLink: string = props.isWPDE ? WPDE_GITHUB_REPO : CENTRAL_NOTICE_EDIT_URL.replace( '{{banner}}', bannerPageName.value );
 
-let isCompiling: Ref<boolean> = ref( false );
-let isCopying: Ref<boolean> = ref( false );
+const isCompiled: Ref<boolean> = computed( () => props.compileInfo !== undefined );
+const isCopied: Ref<boolean> = ref( false );
+
+const bannerCopyTooltip: Ref<string> = computed( () => {
+	if ( props.compileInfo ) {
+
+		if ( isCopied.value ) {
+			return 'Compiled banner copied!';
+		}
+
+		const compiledSizeInKb = Math.round( Number( props.compileInfo.size ) / 1024 );
+		return `Copy ${ compiledSizeInKb } KB Banner Code, compiled ${ relevantTime( props.compileInfo.date.toString() ) }`;
+	}
+	return 'Banner not compiled';
+} );
+
+const isCompiling: Ref<boolean> = ref( false );
+const isCopying: Ref<boolean> = ref( false );
 
 function onCompileBanner( e: Event ): void {
 	e.preventDefault();
@@ -81,12 +96,16 @@ function onCompileBanner( e: Event ): void {
 		if ( result.err ) {
 			alert( result.err );
 		}
+		location.reload();
 		console.log( `Compiled in ${ result.stats.compileTime }` );
 	} );
 }
 
-function bannerCopyHandler( e: Event ): void {
+function onCopyBannerToClipBoard( e: Event ): void {
 	e.preventDefault();
+	if ( !isCompiled.value ) {
+		return;
+	}
 	const bannerFileName = `/compiled-banners/${ bannerPageName.value }.js.wikitext`;
 	isCopying.value = true;
 	fetch( bannerFileName ).then( async response => {
@@ -100,18 +119,16 @@ function bannerCopyHandler( e: Event ): void {
 			return;
 		}
 		const bannerCode = await response.text();
-		await navigator.clipboard.writeText( bannerCode );
+		if ( navigator.clipboard ) {
+			try {
+				await navigator.clipboard.writeText( bannerCode );
+				isCopied.value = true;
+				return;
+			} catch ( error ) {
+				console.error( 'Failed to copy banner code using navigator.clipboard:', error );
+			}
+		}
 	} );
-}
-let onCopyBannerToClipBoard = ( e: Event ): void => e.preventDefault();
-let bannerCopyTooltip = 'Banner not compiled';
-
-if ( !props.isWPDE ) {
-	if ( props.compileInfo ) {
-		const compiledSizeInKb = Math.round( Number( props.compileInfo.size ) / 1024 );
-		onCopyBannerToClipBoard = bannerCopyHandler;
-		bannerCopyTooltip = `Copy ${ compiledSizeInKb } KB Banner Code, compiled ${ relevantTime( props.compileInfo.date.toString() ) }`;
-	}
 }
 
 </script>
