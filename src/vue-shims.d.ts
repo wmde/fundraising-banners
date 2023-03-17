c14bf53f2c6b24934eb7292743fccafbc1fda397
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-03-17 16:16:16 +0100

    Provide global properties from plugins to ts linter

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,6 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ComponentCustomProperties } from 'vue';
+
 // This shim allows TypeScript to process .vue files as modules
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+
+// allow custom properties from our plugins
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $translate: ( key: string ) => string;
+  }
 }
