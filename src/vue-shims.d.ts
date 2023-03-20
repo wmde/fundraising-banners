960905a8090de7be108bb2a1581dffb80f74e583
Abban Dunne <abban.dunne@wikimedia.de>
2023-03-20 06:54:03 +0100

    Make ts plugin type defs work in IDE

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,15 +1,6 @@
-
 // This shim allows TypeScript to process .vue files as modules
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
   export default component;
-}
-
-// allow custom properties from our plugins
-declare module '@vue/runtime-core' {
-  import { ComponentCustomProperties } from 'vue';
-  interface ComponentCustomProperties {
-    $translate: ( key: string ) => string;
-  }
 }
