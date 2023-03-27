bb6b86e392b1f727d55d713dc385ade8938c5746
Abban Dunne <abban.dunne@wikimedia.de>
2023-03-27 12:35:43 +0200

    Start laying out banner

--- banners/desktop/components/MainBanner.vue
+++ banners/desktop/components/MainBanner.vue
@@ -1,9 +1,16 @@
 <template>
-	<div class="wmde-banner-main">
+	<div class="wmde-banner-wrapper">
 		<ButtonClose @click.prevent="onClose"/>
-		<div>WMDE Banner with a message: <span class="wmde-banner-greeting"> {{greeting}} {{ $translate('no-interval-message')}} {{ bannerState }}</span></div>
-		<BannerText />
-		<ProgressBar amount-to-show-on-right="TARGET"/>
+		<div class="wmde-banner-content">
+			<div class="wmde-banner-column-left">
+				<BannerText />
+				<ProgressBar amount-to-show-on-right="TARGET"/>
+			</div>
+			<div class="wmde-banner-column-right">
+				Donation Form
+			</div>
+		</div>
+
 	</div>
 </template>
 
@@ -11,7 +18,7 @@
 import ButtonClose from '@src/components/ButtonClose/ButtonClose.vue';
 import { BannerStates } from '@src/components/BannerConductor/StateMachine/BannerStates';
 import { CloseSources } from '@src/tracking/CloseSources';
-import BannerText from './BannerText.vue';
+import BannerText from '../content/BannerText.vue';
 import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
 
 interface Props {
@@ -32,13 +39,54 @@ function onClose(): void {
 </script>
 
 <style lang="scss">
-/* Import theme */
 @use '../styles/styles';
+@use 'src/themes/treedip/variables/globals';
+@use 'src/themes/treedip/variables/fonts';
+@use 'src/themes/treedip/variables/colors';
 
-/* All layout styles defined in this component will be overridden by the imported theme file */
-.wmde-banner-main {
-	position: relative;
-	color: #008000ff;
+.wmde-banner {
+	&-wrapper {
+		font-size: 14px;
+		font-family: fonts.$ui;
+		box-shadow: 0 3px 0.6em rgba( 60 60 60 / 40% );
+		background-color: colors.$white;
+	}
+
+	&-content {
+		display: flex;
+		flex-direction: row;
+		order: 1;
+		padding: 12px 24px 0;
+	}
+
+	&-column-left {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		flex: 1 1 auto;
+		margin-bottom: 0;
+		overflow-y: hidden;
+		margin-right: 30px;
+		padding: 0 15px;
+		border: 5px solid colors.$primary;
+		border-radius: 9px;
+	}
+
+	&-column-right {
+		order: 2;
+		flex: 0 0 globals.$form-width;
+		display: flex;
+		flex-direction: column;
+		width: globals.$form-width;
+		min-height: 315px;
+		padding: 10px 0;
+	}
+
+	&-message {
+		height: 100%;
+		display: flex;
+		align-items: center;
+	}
 }
 
 </style>
