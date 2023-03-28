d5682b5baa4ed9e5a8eaa10d51afc8005ad222f4
Sperling-0 <tanujadoriya@gmail.com>
2023-03-28 19:08:22 +0200

    Add more tests for SelectCustomAmount

--- test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
+++ test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
@@ -1,41 +1,32 @@
-import { describe, expect, it } from 'vitest';
-import { shallowMount } from '@vue/test-utils';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { shallowMount, VueWrapper } from '@vue/test-utils';
 import SelectCustomAmount from '@src/components/DonationForm/SubComponents/SelectCustomAmount.vue';
 
 describe( 'SelectCustomAmount.vue', () => {
-	it( 'updates the CSS classes when a value is entered', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
+
+	let wrapper: VueWrapper<any>;
+	beforeEach( () => {
+		wrapper = shallowMount( SelectCustomAmount, {
 			props: {
 				placeholder: 'placeholder-text',
 				fieldName: 'fld-custom-amount'
 			}
 		} );
+	} );
 
+	it( 'updates the CSS classes when a value is entered', async () => {
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).setValue( '2500' );
 
 		expect( wrapper.attributes( 'class' ) ).toContain( 'value-entered' );
 	} );
 
 	it( 'updates the CSS classes when a value is entered', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).trigger( 'focus' );
 
 		expect( wrapper.attributes( 'class' ) ).toContain( 'focused' );
 	} );
 
 	it( 'clears the custom amount when the selectedAmount becomes not null', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
 		const textField = wrapper.find<HTMLInputElement>( 'input[type=text]' );
 		await textField.setValue( '2500' );
 		await wrapper.setProps( { selectedAmount: '42' } );
@@ -44,39 +35,18 @@ describe( 'SelectCustomAmount.vue', () => {
 	} );
 
 	it( 'should check radio button when input field is focused', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).trigger( 'focus' );
 
 		expect( wrapper.find<HTMLInputElement>( 'input[type=radio]' ).element.checked ).toBeTruthy();
 	} );
 
 	it( 'should check radio button when input field has custom amount', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).setValue( '2500' );
 
 		expect( wrapper.find<HTMLInputElement>( 'input[type=radio]' ).element.checked ).toBeTruthy();
 	} );
 
 	it( 'should show euro symbol when field is focussed', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( false );
 
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).trigger( 'focus' );
@@ -85,13 +55,6 @@ describe( 'SelectCustomAmount.vue', () => {
 	} );
 
 	it( 'should show euro symbol when field has an entered value', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( false );
 
 		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).setValue( '2500' );
@@ -100,28 +63,27 @@ describe( 'SelectCustomAmount.vue', () => {
 	} );
 
 	it( 'renders field name of radio input', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		expect( wrapper.find( 'input[type=radio]' ).attributes( 'name' ) ).toBe( 'fld-custom-amount' );
 	} );
 
 	it.todo( 'checks if radio element has focus on click', async () => {
-		const wrapper = shallowMount( SelectCustomAmount, {
-			props: {
-				placeholder: 'placeholder-text',
-				fieldName: 'fld-custom-amount'
-			}
-		} );
-
 		await wrapper.find( '.wmde-banner-select-custom-amount-radio' ).trigger( 'click' );
 
-		// TODO find out how to test this
+		// TODO find out how to test this OR if the component code is broken
 		expect( wrapper.find( 'input[type=text]' ) ).toBe( document.activeElement );
 	} );
 
+	it( 'should hide placeholder when the input has focus', async () => {
+		const input = wrapper.find<HTMLInputElement>( 'input[type=text]' );
+
+		await input.trigger( 'focus' );
+
+		expect( input.attributes( 'placeholder' ) ).toBe( '' );
+	} );
+
+	it( 'should render the placeholder when the input in out of focus', async () => {
+		const input = wrapper.find<HTMLInputElement>( 'input[type=text]' );
+
+		expect( input.attributes( 'placeholder' ) ).toBe( 'placeholder-text' );
+	} );
 } );
