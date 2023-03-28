9fffb5e9abce9a3f501b08c30d95f9e1f99b2e70
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-03-28 18:47:56 +0200

    Add more tests for SelectCustomAmount

--- test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
+++ test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
@@ -68,4 +68,60 @@ describe( 'SelectCustomAmount.vue', () => {
 
 		expect( wrapper.find<HTMLInputElement>( 'input[type=radio]' ).element.checked ).toBeTruthy();
 	} );
+
+	it( 'should show euro symbol when field is focussed', async () => {
+		const wrapper = shallowMount( SelectCustomAmount, {
+			props: {
+				placeholder: 'placeholder-text',
+				fieldName: 'fld-custom-amount'
+			}
+		} );
+
+		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( false );
+
+		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).trigger( 'focus' );
+
+		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( true );
+	} );
+
+	it( 'should show euro symbol when field has an entered value', async () => {
+		const wrapper = shallowMount( SelectCustomAmount, {
+			props: {
+				placeholder: 'placeholder-text',
+				fieldName: 'fld-custom-amount'
+			}
+		} );
+
+		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( false );
+
+		await wrapper.find<HTMLInputElement>( 'input[type=text]' ).setValue( '2500' );
+
+		expect( wrapper.find( '.wmde-banner-select-custom-amount-euro-symbol' ).exists() ).toBe( true );
+	} );
+
+	it( 'renders field name of radio input', async () => {
+		const wrapper = shallowMount( SelectCustomAmount, {
+			props: {
+				placeholder: 'placeholder-text',
+				fieldName: 'fld-custom-amount'
+			}
+		} );
+
+		expect( wrapper.find( 'input[type=radio]' ).attributes( 'name' ) ).toBe( 'fld-custom-amount' );
+	} );
+
+	it.todo( 'checks if radio element has focus on click', async () => {
+		const wrapper = shallowMount( SelectCustomAmount, {
+			props: {
+				placeholder: 'placeholder-text',
+				fieldName: 'fld-custom-amount'
+			}
+		} );
+
+		await wrapper.find( '.wmde-banner-select-custom-amount-radio' ).trigger( 'click' );
+
+		// TODO find out how to test this
+		expect( wrapper.find( 'input[type=text]' ) ).toBe( document.activeElement );
+	} );
+
 } );
