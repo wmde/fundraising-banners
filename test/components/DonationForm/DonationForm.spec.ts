7257c6bb3ce75d3b10c41a846d85b1cfc7de1e96
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-03-29 17:51:03 +0200

    Start oulining the donation form test

--- test/components/DonationForm/DonationForm.spec.ts
+++ test/components/DonationForm/DonationForm.spec.ts
@@ -1,5 +1,29 @@
 import { describe, it } from 'vitest';
+import { mount } from '@vue/test-utils';
+import DonationForm from '@src/components/DonationForm/DonationForm.vue';
+import { DonationFormItems } from '@src/utils/FormItemsBuilder/DonationFormItems';
+
+const formItems: DonationFormItems = {
+	addressType: [],
+	amounts: [ { value: '0', label: '€0', className: '' } ],
+	intervals: [],
+	paymentMethods: []
+};
 
 describe( 'DonationForm.vue', () => {
-	it.todo( 'updates the interval when one is selected' );
+	it.todo( 'updates the interval when one is selected', () => {
+		const wrapper = mount( DonationForm, {
+			props: {
+				formUrl: 'https://example.com',
+				customAmountPlaceholder: 'custom-amount-placeholder'
+			},
+			global: {
+				provide: { formItems: formItems }
+			}
+		} );
+
+		// TODO: find input element in first interval and trigger its select event
+
+		// TODO: Check if SubmitValues component (to be done) has the right interval
+	} );
 } );
