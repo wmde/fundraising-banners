57ba6331253c0ecee1f60430e69b1e94c1e4409a
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-03-29 17:37:25 +0200

    Add remaining tests for useFormModel

--- test/unit/utils/FormModel/services/useFormModel.spec.ts
+++ test/unit/utils/FormModel/services/useFormModel.spec.ts
@@ -26,14 +26,14 @@ describe( 'useFormModel', () => {
 		expect( model.addressType.value ).toBe( AddressTypes.NO.value );
 	} );
 
-	it( 'disables all recurring intervals if payment method is SOFORT', async function () {
+	it( 'disables all recurring intervals if payment method is SOFORT', function () {
 		const model = useFormModel();
 		model.paymentMethod.value = PaymentMethods.SOFORT.value;
 
 		expect( model.disabledIntervals.value ).toBe( RecurringIntervals );
 	} );
 
-	it( 'allows all intervals for any other payment method than SOFORT', async function () {
+	it( 'allows all intervals for any other payment method than SOFORT', function () {
 		const model = useFormModel();
 
 		expect( model.disabledIntervals.value ).toEqual( [] );
@@ -43,28 +43,28 @@ describe( 'useFormModel', () => {
 		expect( model.disabledIntervals.value ).toEqual( [] );
 	} );
 
-	it( 'disables SOFORT when the interval is set an recurring interval', async function () {
+	it( 'disables SOFORT when the interval is set an recurring interval', function () {
 		const model = useFormModel();
 		model.interval.value = Intervals.QUARTERLY.value;
 
 		expect( model.disabledPaymentMethods.value ).toEqual( [ PaymentMethods.SOFORT.value ] );
 	} );
 
-	it( 'allows all payment methods on one time interval', async function () {
+	it( 'allows all payment methods on one time interval', function () {
 		const model = useFormModel();
 		model.interval.value = Intervals.ONCE.value;
 
 		expect( model.disabledPaymentMethods.value ).toEqual( [] );
 	} );
 
-	it( 'disables anonymous address type when direct debit was selected', async function () {
+	it( 'disables anonymous address type when direct debit was selected', function () {
 		const model = useFormModel();
 		model.paymentMethod.value = PaymentMethods.DIRECT_DEBIT.value;
 
 		expect( model.disabledAddressTypes.value ).toEqual( [ AddressTypes.NO.value ] );
 	} );
 
-	it( 'allows all address types when any other payment type than direct debit was selected', async function () {
+	it( 'allows all address types when any other payment type than direct debit was selected', function () {
 		const model = useFormModel();
 		model.paymentMethod.value = PaymentMethods.PAYPAL.value;
 
@@ -75,11 +75,23 @@ describe( 'useFormModel', () => {
 		const model = useFormModel();
 		model.amount.value = '';
 		model.customAmount.value = '999';
+		await nextTick();
 
 		model.amount.value = '500';
 		await nextTick();
 
 		expect( model.customAmount.value ).toEqual( '' );
 	} );
-}
-);
+
+	it( 'should clear amount when custom amount changes', async function () {
+		const model = useFormModel();
+		model.amount.value = '555';
+		model.customAmount.value = '';
+		await nextTick();
+
+		model.customAmount.value = '500';
+		await nextTick();
+
+		expect( model.amount.value ).toEqual( '' );
+	} );
+} );
