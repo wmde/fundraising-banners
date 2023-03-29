57f276a302e73330d12f97363cabd3db6bd86647
Sperling-0 <tanujadoriya@gmail.com>
2023-03-29 17:04:48 +0200

    Fix ci issues

--- test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
+++ test/components/DonationForm/SubComponents/SelectCustomAmount.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { shallowMount, VueWrapper } from '@vue/test-utils';
 import SelectCustomAmount from '@src/components/DonationForm/SubComponents/SelectCustomAmount.vue';
 
