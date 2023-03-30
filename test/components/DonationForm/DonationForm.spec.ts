8ff07e7b097e0ddfce14f5e79e251732fb1d2460
Abban Dunne <abban.dunne@wikimedia.de>
2023-03-30 07:20:17 +0200

    Add DonationForm tests

--- test/components/DonationForm/DonationForm.spec.ts
+++ test/components/DonationForm/DonationForm.spec.ts
@@ -1,29 +1,54 @@
-import { describe, it } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { mount, VueWrapper } from '@vue/test-utils';
 import DonationForm from '@src/components/DonationForm/DonationForm.vue';
 import { DonationFormItems } from '@src/utils/FormItemsBuilder/DonationFormItems';
+import { Intervals } from '@src/utils/FormItemsBuilder/fields/Intervals';
+import { AddressTypes } from '@src/utils/FormItemsBuilder/fields/AddressTypes';
+import { PaymentMethods } from '@src/utils/FormItemsBuilder/fields/PaymentMethods';
 
 const formItems: DonationFormItems = {
-	addressType: [],
-	amounts: [ { value: '0', label: '€0', className: '' } ],
-	intervals: [],
-	paymentMethods: []
+	addressType: [ AddressTypes.NO, AddressTypes.EMAIL ],
+	amounts: [
+		{ value: '1', label: '€1', className: 'amount-1' },
+		{ value: '5', label: '€5', className: 'amount-5' }
+	],
+	intervals: [ Intervals.ONCE, Intervals.MONTHLY ],
+	paymentMethods: [ PaymentMethods.PAYPAL, PaymentMethods.CREDIT_CARD ]
 };
 
 describe( 'DonationForm.vue', () => {
-	it.todo( 'updates the interval when one is selected', () => {
-		const wrapper = mount( DonationForm, {
+	let wrapper: VueWrapper<any>;
+
+	beforeEach( () => {
+		wrapper = mount( DonationForm, {
 			props: {
 				formUrl: 'https://example.com',
 				customAmountPlaceholder: 'custom-amount-placeholder'
 			},
 			global: {
-				provide: { formItems: formItems }
+				provide: { formItems: formItems },
+				mocks: {
+					$translate: ( key: string ) => key
+				}
 			}
 		} );
+	} );
 
-		// TODO: find input element in first interval and trigger its select event
+	it( 'updates the interval when one is selected', async () => {
+		await wrapper.find( '.interval-1 input' ).trigger( 'change' );
 
-		// TODO: Check if SubmitValues component (to be done) has the right interval
+		expect( wrapper.find<HTMLInputElement>( 'input[name=interval]' ).element.value ).toBe( '1' );
+	} );
+
+	it( 'updates the amount when one is selected', async () => {
+		await wrapper.find( '.amount-5 input' ).trigger( 'change' );
+
+		expect( wrapper.find<HTMLInputElement>( 'input[name=amount]' ).element.value ).toBe( '5' );
+	} );
+
+	it( 'updates the payment method when one is selected', async () => {
+		await wrapper.find( '.payment-PPL input' ).trigger( 'change' );
+
+		expect( wrapper.find<HTMLInputElement>( 'input[name=paymentType]' ).element.value ).toBe( 'PPL' );
 	} );
 } );
