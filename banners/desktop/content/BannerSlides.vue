551b2d09c6ee5710250837f1c74471c7000c9d7d
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-04-03 19:41:04 +0200

    Small demo how how to wrap dynamic text slides

--- banners/desktop/content/BannerSlides.vue
+++ banners/desktop/content/BannerSlides.vue
@@ -1,6 +1,10 @@
 <template>
-	<div> TODO add slides here </div>
+	<KeenSlider>
+		<template #slides-1><div>First slide</div></template>
+		<template #slides-2><div>Second slide</div></template>
+	</KeenSlider>
 </template>
 
 <script setup lang="ts">
+import KeenSlider from '@src/components/Slider/KeenSlider.vue';
 </script>
