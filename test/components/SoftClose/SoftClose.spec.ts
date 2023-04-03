b829159916c4cf834c9d8bcc9738db357be4817a
Abban Dunne <abban.dunne@wikimedia.de>
2023-04-03 07:16:06 +0200

    Add remaining SoftClose tests

--- test/components/SoftClose/SoftClose.spec.ts
+++ test/components/SoftClose/SoftClose.spec.ts
@@ -1,8 +1,17 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, afterEach, beforeEach, vi } from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import SoftClose from '@src/components/SoftClose/SoftClose.vue';
 
 describe( 'SoftClose', function () {
+
+	beforeEach( () => {
+		vi.useFakeTimers();
+	} );
+
+	afterEach( () => {
+		vi.restoreAllMocks();
+	} );
+
 	it( 'should emit close event when user clicks close button', function () {
 		const wrapper = shallowMount( SoftClose, {
 			global: {
@@ -31,8 +40,40 @@ describe( 'SoftClose', function () {
 		expect( wrapper.emitted( 'maybeLater' ).length ).toBe( 1 );
 	} );
 
-	// hint: https://vitest.dev/guide/mocking.html#timers
-	it.todo( 'should emit timeOutClose when the internal countdown is <= 1' );
+	it( 'should display the remaining seconds decremented every second', async () => {
+		const wrapper = shallowMount( SoftClose, {
+			global: {
+				mocks: {
+					$translate: ( key: string, templateTags: Record<string, string | number> = {} ) => {
+						return templateTags === undefined ? key : templateTags.seconds;
+					}
+				}
+			}
+		} );
 
-	it.todo( 'should display the remaining seconds' );
+		const seconds = wrapper.find( '.wmde-banner-soft-close-countdown-text-wrapper' );
+
+		expect( seconds.text() ).toBe( '15' );
+
+		await vi.advanceTimersByTimeAsync( 1000 );
+		expect( seconds.text() ).toBe( '14' );
+
+		await vi.advanceTimersByTimeAsync( 1000 );
+		expect( seconds.text() ).toBe( '13' );
+
+		await vi.advanceTimersByTimeAsync( 1000 );
+		expect( seconds.text() ).toBe( '12' );
+	} );
+
+	it( 'should emit timeOutClose when the internal countdown is <= 1', () => {
+		const wrapper = shallowMount( SoftClose, {
+			global: {
+				mocks: {
+					$translate: ( key: string ) => key
+				}
+			}
+		} );
+		vi.runAllTimers();
+		expect( wrapper.emitted( 'timeOutClose' ).length ).toBe( 1 );
+	} );
 } );
