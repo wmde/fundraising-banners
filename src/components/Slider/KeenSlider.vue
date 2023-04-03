8c095dffa011b153afdf8344f63e106fb5146b41
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-04-03 19:41:04 +0200

    Add proof of concept for slides
    Slides component for each banner uses different slot names for each
    slide. Those slot names will get picked up in the KeenSlider component
    an wrapped in divs with class names that are necessary for the KeenSlider
    library.

---
+++ src/components/Slider/KeenSlider.vue
@@ -0,0 +1,26 @@
+<template>
+	<template v-for="(slotName, idx) in usedSlotNames" :key="slotName">
+		<div class="keen-slider__slide wmde-banner-slide">
+			<div :class="[
+				'keen-slider__slide-content',
+				'wmde-banner-slide-content',
+				{'wmde-banner-slide--current': currentSlide === idx}
+				]">
+				<slot :name="slotName" />
+			</div>
+		</div>
+	</template>
+
+</template>
+
+<script setup lang="ts">
+import { useSlots, ref } from 'vue';
+
+const slots = useSlots();
+
+// TODO connect this to KeenSlider
+const currentSlide = ref<number>( 0 );
+
+const usedSlotNames = Object.keys( slots );
+
+</script>
