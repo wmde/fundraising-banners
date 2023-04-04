cd9c5015d9b3b28f348ac0cfd1739b4acfd307e5
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-04-04 10:40:00 +0200

    Add more tests for useDisplySwitch

--- test/components/composables/useDisplaySwitch.spec.ts
+++ test/components/composables/useDisplaySwitch.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { useDisplaySwitch } from '@src/components/composables/useDisplaySwitch';
 import { defineComponent } from 'vue';
 import { shallowMount } from '@vue/test-utils';
@@ -34,5 +34,35 @@ describe( 'useDisplaySwitch', () => {
 
 		expect( wrapper.vm.onLargeScreen ).toBe( false );
 
+	} );
+
+	it( 'triggers event handler on window resize', () => {
+		Object.defineProperty( window, 'innerWidth', { writable: true, configurable: true, value: 100 } );
+
+		const wrapper = shallowMount( TestComponent, { props: { minWidth: 100 } } );
+
+		Object.defineProperty( window, 'innerWidth', { value: 101 } );
+		window.dispatchEvent( new Event( 'resize' ) );
+
+		expect( wrapper.vm.onLargeScreen ).toBe( true );
+	} );
+
+	it( 'adds event handler on mount', () => {
+		Object.defineProperty( window, 'innerWidth', { writable: true, configurable: true, value: 100 } );
+		Object.defineProperty( window, 'addEventListener', { writable: true, configurable: true, value: vi.fn() } );
+
+		shallowMount( TestComponent, { props: { minWidth: 100 } } );
+
+		expect( window.addEventListener ).toHaveBeenCalled();
+	} );
+
+	it( 'removes event handler on unmount', () => {
+		Object.defineProperty( window, 'innerWidth', { writable: true, configurable: true, value: 100 } );
+		Object.defineProperty( window, 'removeEventListener', { writable: true, configurable: true, value: vi.fn() } );
+
+		const wrapper = shallowMount( TestComponent, { props: { minWidth: 100 } } );
+		wrapper.unmount();
+
+		expect( window.removeEventListener ).toHaveBeenCalled();
 	} );
 } );
