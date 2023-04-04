f82dffcc57244dd15f2e1f2692aad583a2f3fb31
Sperling-0 <tanujadoriya@gmail.com>
2023-04-04 11:34:00 +0200

    Add HTML to KeenSlider component
    - State and event handlers from old code are not finished

--- src/components/Slider/KeenSlider.vue
+++ src/components/Slider/KeenSlider.vue
@@ -1,20 +1,56 @@
 <template>
-	<template v-for="(slotName, idx) in usedSlotNames" :key="slotName">
-		<div class="keen-slider__slide wmde-banner-slide">
-			<div :class="[
-				'keen-slider__slide-content',
-				'wmde-banner-slide-content',
-				{'wmde-banner-slide--current': currentSlide === idx}
-				]">
-				<slot :name="slotName" />
+	<div class="wmde-banner-slider-container">
+		<div class="wmde-banner-navigation-wrapper">
+			<div
+				class="keen-slider wmde-banner-slider"
+				@mousedown="stopAutoplay"
+				@touchstart="stopAutoplay"
+			>
+				<template v-for="( slotName, idx ) in usedSlotNames" :key="slotName">
+					<div class="keen-slider__slide wmde-banner-slide">
+						<div :class="[
+							'keen-slider__slide-content',
+							'wmde-banner-slide-content',
+							{ 'wmde-banner-slide--current': currentSlide === idx }
+						]">
+							<slot :name="slotName"/>
+						</div>
+					</div>
+				</template>
+			</div>
+
+			<a v-if="previous" href="#" class="wmde-banner-slider-navigation-previous" @click="gotoPreviousSlide">
+				{{ previous }}
+			</a>
+
+			<a v-if="next" href="#" class="wmde-banner-slider-navigation-next" @click="gotoNextSlide">
+				{{ next }}
+			</a>
+
+			<div class="wmde-banner-slider-pagination">
+				<button
+					v-for="(slotName, idx) in usedSlotNames" :key="slotName"
+					:class="[ 'wmde-banner-slider-pagination-dot', { 'is-active': currentSlide === idx } ]"
+					@click="() => goToSlide( idx )"
+				/>
 			</div>
 		</div>
-	</template>
+	</div>
 
 </template>
 
 <script setup lang="ts">
-import { useSlots, ref } from 'vue';
+import { ref, useSlots } from 'vue';
+import { KeenSliderOptions } from 'keen-slider/vue';
+
+interface Props {
+	interval: number;
+	sliderOptions?: KeenSliderOptions;
+}
+
+withDefaults( defineProps<Props>(), {
+	sliderOptions: () => ( {} )
+} );
 
 const slots = useSlots();
 
@@ -23,4 +59,8 @@ const currentSlide = ref<number>( 0 );
 
 const usedSlotNames = Object.keys( slots );
 
+const stopAutoplay = () => {
+	clearInterval( timer );
+	sliderPlayingState = SliderPlayingStates.STOPPED;
+};
 </script>
