bf94c3d61985e522dd9ce207947f3ee8cd3e55c2
Sperling-0 <tanuja.doriya@wikimedia.de>
2023-04-05 16:17:16 +0200

    Prepare page switching in MultiStepDonation component
    The component will pass the control to the controller.

--- src/components/DonationForm/MultiStepDonation.vue
+++ src/components/DonationForm/MultiStepDonation.vue
@@ -1,13 +1,63 @@
 <template>
-
+	<div class="wmde-banner-multistep-donation-form" ref="container">
+		<template v-for="( slotName, idx ) in usedSlotNames" :key="slotName">
+			<div class="keen-slider__slide wmde-banner-donation-form-page">
+				<div :class="[
+								'keen-slider__slide-content',
+								'wmde-banner-donation-form-page-content',
+								{ 'wmde-banner-donation-form-page-content--current': currentFormPage === idx }
+							]">
+					<slot
+						:name="slotName"
+						@submit="onSubmit"
+						@next="onNext"
+						@previous="onPrevious"
+					/>
+				</div>
+			</div>
+		</template>
+	</div>
 </template>
 
-<script>
-export default {
-	name: 'MultiStepDonation'
+<script setup lang="ts">
+
+import { ref, useSlots } from 'vue';
+import { useKeenSlider } from 'keen-slider/vue';
+import { FormController } from '@src/utils/FormController/FormController';
+
+const currentFormPage = ref<number>( 0 );
+
+interface Props {
+	showErrorScrollLink?: boolean;
+	formController: FormController;
+}
+
+const props = withDefaults( defineProps<Props>(), {
+	showErrorScrollLink: false
+} );
+
+const slots = useSlots();
+
+const [ container, slider ] = useKeenSlider( {
+	initial: 0
+} );
+
+const usedSlotNames = Object.keys( slots );
+
+const onSubmit = ( extraData: Record<string, string> = {} ): void => {
+	props.formController.submit( { pageNumber: currentFormPage.value, extraData } );
 };
+
+const onPrevious = (): void => {
+	props.formController.previous();
+};
+
+const onNext = (): void => {
+	props.formController.next();
+};
+
 </script>
 
-<style scoped>
+<style lang="scss">
 
 </style>
