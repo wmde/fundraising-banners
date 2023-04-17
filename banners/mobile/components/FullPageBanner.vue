d37e85a336ccfe95bbce85435d8860864446d335
Abban Dunne <abban.dunne@wikimedia.de>
2023-04-17 10:27:47 +0200

    Update mobile breakpoints

--- banners/mobile/components/FullPageBanner.vue
+++ banners/mobile/components/FullPageBanner.vue
@@ -1,18 +1,30 @@
 <template>
-	<div class="wmde-banner-main">
-		<ButtonClose @click.prevent="$emit( 'close' )"/>
-		<div class="wmde-banner-content">
-			<div class="wmde-banner-column-left">
-				<BannerText v-if="onLargeScreen"/>
-				<ProgressBar amount-to-show-on-right="TARGET"/>
-			</div>
-			<div class="wmde-banner-column-right">
-				<MultiStepDonation
-					:form-controller="formController"
-					:forms="forms"
-				/>
-			</div>
+	<div class="wmde-banner-full">
+		<ButtonClose class="wmde-banner-full-close" @click.prevent="$emit( 'close' )"/>
+		<div class="wmde-banner-full-info">
+			<BannerText v-if="onLargeScreen"/>
+			<ProgressBar amount-to-show-on-right="TARGET"/>
 		</div>
+		<div className="wmde-banner-full-call-to-action">
+			Jetzt sind Sie <span className="wmde-banner-full-call-to-action-optional-text">in Deutschland</span> gefragt.
+		</div>
+		<MultiStepDonation
+			:form-controller="formController"
+			:forms="forms"
+		/>
+
+		<div class="wmde-banner-full-small-print">
+			<span>
+				<a
+					class="application-of-funds-link t-use-of-funds-link"
+					href="{`https://spenden.wikimedia.de/use-of-funds?${ props.trackingParams }`}"
+					onClick="{ props.toggleFundsModal }"
+				>
+					Wohin geht meine Spende?
+				</a>
+			</span>
+		</div>
+
 		<BannerFooter />
 	</div>
 </template>
@@ -43,38 +55,89 @@ const onLargeScreen = useDisplaySwitch( 1300 );
 </script>
 
 <style lang="scss">
-@use 'src/themes/treedip/variables/globals';
-@use 'src/themes/treedip/variables/colors';
+@use 'src/themes/mikings/variables/globals';
+@use 'src/themes/mikings/variables/colors';
+@use 'src/themes/mikings/variables/breakpoints';
 
 .wmde-banner {
-	&-content {
-		display: flex;
-		flex-direction: row;
-		order: 1;
-		padding: 12px 24px 0;
-	}
+	&-full {
+		border: 2px solid colors.$primary;
+		background: colors.$white;
 
-	&-column-left {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		flex: 1 1 auto;
-		margin-bottom: 0;
-		overflow-y: hidden;
-		margin-right: 30px;
-		padding: 0 15px;
-		border: 5px solid colors.$primary;
-		border-radius: 9px;
-	}
+		p {
+			padding-bottom: 16px;
+		}
 
-	&-column-right {
-		order: 2;
-		flex: 0 0 globals.$form-width;
-		display: flex;
-		flex-direction: column;
-		width: globals.$form-width;
-		min-height: 315px;
-		padding: 10px 0;
+		&-close {
+			position: absolute;
+			top: 16px;
+			right: 16px;
+			height: 25px;
+			background: colors.$white;
+
+			&:hover {
+				cursor: pointer;
+			}
+
+			.close-button {
+				text-decoration: underline;
+			}
+		}
+
+		&-info {
+			padding: 16px;
+		}
+
+		&-call-to-action {
+			position: relative;
+			color: colors.$white;
+			background: colors.$primary;
+			font-weight: bold;
+			height: 31px;
+			line-height: 31px;
+			text-align: center;
+
+			&-optional-text {
+				display: none;
+
+				@include breakpoints.tablet-portrait-up {
+					display: inline;
+				}
+			}
+
+			&::after {
+				content: '';
+				position: absolute;
+				bottom: -4px;
+				left: 50%;
+				margin-left: -4px;
+				width: 0;
+				height: 0;
+				border-style: solid;
+				border-width: 4px 4px 0;
+				border-color: colors.$primary transparent transparent transparent;
+			}
+		}
+
+		.banner-text-title {
+			margin-right: 30px;
+		}
+
+		&-small-print {
+			text-align: center;
+			font-size: 12px;
+			margin-bottom: 16px;
+
+			a {
+				color: colors.$gray;
+
+				&:hover,
+				&:focus {
+					text-decoration: underline;
+				}
+			}
+		}
 	}
 }
+
 </style>
--- banners/mobile/content/BannerSlides.vue
+++ banners/mobile/content/BannerSlides.vue
@@ -1,23 +1,38 @@
 <template>
-	<KeenSlider :interval="5000" :with-navigation="true" :start="start">
-		<template #slides-1><div>
-			<p class="headline">
-				<InfoIcon fill="#990a00"/> <strong> An alle, die Wikipedia in Deutschland nutzen </strong>
-			</p>
-			<p>
-				Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
-				Am heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
-				zu unterstützen. {{campaignDaySentence }}</p>
-		</div></template>
-		<template #slides-2><div>
-			<p><span class="wmde-banner-text-animated-highlight">{{ visitorsVsDonorsSentence }}</span>
-				Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
-				Die durchschnittliche Spende beträgt 22,66&nbsp;€, doch bereits eine Spende von 5&nbsp;€ hilft uns weiter.</p>
-		</div></template>
-		<template #slides-3><div>
-			<p>Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden
-				Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!</p>
-		</div></template>
+	<KeenSlider :interval="5000" :with-navigation="false" :start="start">
+		<template #slides-1>
+			<div>
+				<p>Unser Spendenziel: {{ goalDonationSum }} Millionen Euro</p>
+				<ProgressBar amount-to-show-on-right="TARGET"/>
+			</div>
+		</template>
+		<template #slides-2>
+			<div>
+				<p>An alle, die Wikipedia in Deutschland nutzen:<br/>
+					Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
+					Am heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
+					zu sichern.</p>
+			</div>
+		</template>
+		<template #slides-3>
+			<div>
+				<p>{{ campaignDaySentence }} Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf. Wikipedia
+					wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert.</p>
+			</div>
+		</template>
+		<template #slides-4>
+			<div>
+				<p>Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
+					entwickeln. <span v-if="visitorsVsDonorsSentence !== ''" class="wmde-banner-text-animated-highlight">{{ visitorsVsDonorsSentence }}</span></p>
+			</div>
+		</template>
+		<template #slides-5>
+			<div>
+				<p>Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in
+					diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen Sie sich doch bitte
+						eine Minute Zeit und geben Sie etwas zurück. Vielen Dank!</strong></p>
+			</div>
+		</template>
 	</KeenSlider>
 </template>
 
@@ -25,7 +40,7 @@
 import KeenSlider from '@src/components/Slider/KeenSlider.vue';
 import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
 import { inject } from 'vue';
-import InfoIcon from '@src/components/Icons/InfoIcon.vue';
+import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
 
 interface Props {
 	start: boolean
@@ -33,5 +48,10 @@ interface Props {
 
 defineProps<Props>();
 
-const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence }: DynamicContent = inject( 'dynamicCampaignText' );
+const {
+	currentDayName,
+	campaignDaySentence,
+	visitorsVsDonorsSentence,
+	goalDonationSum
+}: DynamicContent = inject( 'dynamicCampaignText' );
 </script>
