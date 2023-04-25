6ed1116051ca9dd2ce83da9a6f5b62978793dd4e
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-04-25 15:32:30 +0200

    Start refactoring of locale-specifc imports

--- banners/desktop/banner_ctrl.ts
+++ banners/desktop/banner_ctrl.ts
@@ -11,6 +11,13 @@ import { WindowMediaWiki } from '@src/page/MediaWiki/WindowMediaWiki';
 import { SkinFactory } from '@src/page/skin/SkinFactory';
 import { WindowSizeIssueChecker } from '@src/utils/SizeIssueChecker/WindowSizeIssueChecker';
 import TranslationPlugin from '@src/TranslationPlugin';
+import { Translator } from '@src/Translator';
+import DynamicTextPlugin from '@src/DynamicTextPlugin';
+import { LocalImpressionCount } from '@src/utils/LocalImpressionCount';
+
+// Locale-specific imports
+import messages from './messages';
+import { LocaleFactoryDe } from '@src/utils/LocaleFactory/LocaleFactoryDe';
 
 // Channel specific form setup
 import { createFormItems } from './form_items';
@@ -18,27 +25,13 @@ import { createFormActions } from '@src/createFormActions';
 import { FormControllerCtrl } from './FormControllerCtrl';
 import { useFormModel } from '@src/components/composables/useFormModel';
 
-// Change for EN banners
-import messages from './messages';
-import { Translator } from '@src/Translator';
-import DynamicTextPlugin from '@src/DynamicTextPlugin';
-import { LocalImpressionCount } from '@src/utils/LocalImpressionCount';
-import { Formatters } from '@src/utils/DynamicContent/Formatters';
-import { CurrencyDe } from '@src/utils/DynamicContent/formatters/CurrencyDe';
-import { OrdinalDe } from '@src/utils/DynamicContent/formatters/OrdinalDe';
-import { IntegerDe } from '@src/utils/DynamicContent/formatters/IntegerDe';
-import { DeJSONFundsContentLoader } from '@src/utils/UseOfFunds/DeJSONFundsContentLoader';
-
-const useOfFundsContent = ( new DeJSONFundsContentLoader() ).getContent();
+const localeFactory = new LocaleFactoryDe();
+const useOfFundsContent = localeFactory.getUseOfFundsLoader().getContent();
 const translator = new Translator( messages );
 
 // This is channel specific and must be changed for wp.de banners
 const mediaWiki = new WindowMediaWiki();
 const page = new PageOrg( mediaWiki, ( new SkinFactory( mediaWiki ) ).getSkin(), new WindowSizeIssueChecker() );
-
-// This is language-specific and must be changed for EN banners
-const currencyFormatter = new CurrencyDe();
-const formatters: Formatters = { currency: currencyFormatter, ordinal: new OrdinalDe(), integer: new IntegerDe() };
 
 const impressionCount = new LocalImpressionCount( page.getTracking().keyword );
 
@@ -61,10 +54,12 @@ app.use( TranslationPlugin, translator );
 app.use( DynamicTextPlugin, {
 	campaignParameters: page.getCampaignParameters(),
 	date: new Date(),
-	formatters,
+	formatters: localeFactory.getFormatters(),
 	impressionCount,
 	translator
 } );
+
+const currencyFormatter = localeFactory.getCurrencyFormatter();
 
 app.provide( 'currencyFormatter', currencyFormatter );
 app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
