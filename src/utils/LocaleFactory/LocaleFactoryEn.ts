53c95332433293510aa66c608a9ab73fbe03b1e9
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-04-25 16:28:33 +0200

    Add new English LocaleFactory for English UoF

---
+++ src/utils/LocaleFactory/LocaleFactoryEn.ts
@@ -0,0 +1,34 @@
+import { LocaleFactory } from '@src/utils/LocaleFactory';
+import { CurrencyEn } from '@src/utils/DynamicContent/formatters/CurrencyEn';
+import { Currency } from '@src/utils/DynamicContent/formatters/Currency';
+import { Formatters } from '@src/utils/DynamicContent/Formatters';
+import { FundsContentLoader } from '@src/utils/UseOfFunds/FundsContentLoader';
+import { OrdinalEn } from '@src/utils/DynamicContent/formatters/OrdinalEn';
+import { IntegerEn } from '@src/utils/DynamicContent/formatters/IntegerEn';
+// TODO import from @environment instead of src
+import { EnJSONFundsContentLoader } from '@src/utils/UseOfFunds/EnJSONFundsContentLoader';
+
+export class LocaleFactoryEn implements LocaleFactory {
+	private readonly _currencyFormatter: Currency;
+
+	public constructor() {
+		this._currencyFormatter = new CurrencyEn();
+	}
+
+	public getCurrencyFormatter(): Currency {
+		return this._currencyFormatter;
+	}
+
+	public getFormatters(): Formatters {
+		return {
+			currency: this._currencyFormatter,
+			ordinal: new OrdinalEn(),
+			integer: new IntegerEn()
+		};
+	}
+
+	public getUseOfFundsLoader(): FundsContentLoader {
+		return new EnJSONFundsContentLoader();
+	}
+
+}
