bf35501d9ee35c7f65053894961d216a458de9c7
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-04-26 18:19:28 +0200

    Add WPDE Mobile implementation

---
+++ banners/wpde_mobile/components/BannerCtrl.vue
@@ -0,0 +1,185 @@
+<template>
+	<div class="wmde-banner-wrapper" :class="contentState">
+		<MiniBanner
+			@close="onCloseMiniBanner"
+			@show-full-page-banner="onshowFullPageBanner"
+		>
+			<template #banner-slides>
+				<BannerSlides :play="slideshowShouldPlay"/>
+			</template>
+		</MiniBanner>
+
+		<FullPageBanner
+			@showFundsModal="isFundsModalVisible = true"
+			@close="() => onClose( CloseSources.FollowUpBanner )"
+		>
+			<template #banner-text>
+				<BannerText/>
+			</template>
+
+			<template #progress>
+				<ProgressBar amount-to-show-on-right="TARGET"/>
+			</template>
+
+			<template #donation-form>
+				<MultiStepDonation :form-controller="formController">
+
+					<template #form-page-1="{ pageIndex, submit, next, previous }: any">
+						<MainDonationForm :page-index="pageIndex" @submit="submit" @next="next" @previous="previous"/>
+					</template>
+
+					<template #form-page-2="{ pageIndex, submit, next, previous }: any">
+						<UpgradeToYearlyButtonForm :page-index="pageIndex" @submit="submit" @next="next" @previous="previous"/>
+					</template>
+
+				</MultiStepDonation>
+			</template>
+
+			<template #footer>
+				<BannerFooter @showFundsModal="isFundsModalVisible = true" />
+			</template>
+		</FullPageBanner>
+
+		<SoftClose
+			v-if="contentState === ContentStates.SoftClosing"
+			@close="() => onClose( CloseSources.SoftCloseBannerRejected )"
+			@maybe-later="() => onClose( CloseSources.MaybeLater )"
+			@time-out-close="() => onClose( CloseSources.TimeOut )"
+		/>
+
+		<FundsModal
+			:content="useOfFundsContent"
+			:is-funds-modal-visible="isFundsModalVisible"
+			@hideFundsModal="onHideFundsModal"
+        />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { BannerStates } from '@src/components/BannerConductor/StateMachine/BannerStates';
+import { CloseSources } from '@src/tracking/CloseSources';
+import SoftClose from '@src/components/SoftClose/SoftClose.vue';
+import { computed, nextTick, ref, watch } from 'vue';
+import FullPageBanner from './FullPageBanner.vue';
+import { FormController } from '@src/utils/FormController/FormController';
+import MiniBanner from './MiniBanner.vue';
+import FundsModal from '@src/components/UseOfFunds/FundsModal.vue';
+import { UseOfFundsContent as useOfFundsContentInterface } from '@src/domain/UseOfFunds/UseOfFundsContent';
+import { UseOfFundsCloseSources } from '@src/components/UseOfFunds/UseOfFundsCloseSources';
+import { PageScroller } from '@src/utils/PageScroller/PageScroller';
+import MainDonationForm from '@src/components/DonationForm/Forms/MainDonationForm.vue';
+import MultiStepDonation from '@src/components/DonationForm/MultiStepDonation.vue';
+import BannerText from '../content/BannerText.vue';
+import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
+import UpgradeToYearlyButtonForm from '@src/components/DonationForm/Forms/UpgradeToYearlyButtonForm.vue';
+import BannerSlides from '../content/BannerSlides.vue';
+import BannerFooter from '@src/components/Footer/BannerFooter.vue';
+
+enum ContentStates {
+	Mini = 'wmde-banner-wrapper--mini',
+	FullPage = 'wmde-banner-wrapper--full-page',
+	SoftClosing = 'wmde-banner-wrapper--soft-closing'
+}
+
+interface Props {
+	bannerState: BannerStates;
+	formController: FormController;
+	useOfFundsContent: useOfFundsContentInterface;
+	pageScroller: PageScroller;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits( [ 'bannerClosed', 'bannerContentChanged' ] );
+
+const isFundsModalVisible = ref<boolean>( false );
+const slideShowStopped = ref<boolean>( false );
+const slideshowShouldPlay = computed( () => props.bannerState === BannerStates.Visible && !slideShowStopped.value );
+const contentState = ref<ContentStates>( ContentStates.Mini );
+
+watch( contentState, async () => {
+	// Wait a tick in order to let the content re-render before notifying the parent
+	await nextTick();
+	emit( 'bannerContentChanged' );
+} );
+
+function onCloseMiniBanner(): void {
+	contentState.value = ContentStates.SoftClosing;
+}
+
+function onClose( closeSource: CloseSources ): void {
+	emit( 'bannerClosed', closeSource );
+}
+
+function onshowFullPageBanner(): void {
+	slideShowStopped.value = true;
+	contentState.value = ContentStates.FullPage;
+}
+
+const onHideFundsModal = ( payload: { source: UseOfFundsCloseSources } ): void => {
+	props.pageScroller.scrollIntoView( payload.source === UseOfFundsCloseSources.callToAction ?
+		'.wmde-banner-form' :
+		'.wmde-banner-full-small-print .wmde-banner-footer-usage-link'
+	);
+	isFundsModalVisible.value = false;
+};
+
+</script>
+
+<style lang="scss">
+@use 'src/themes/Mikings/variables/globals';
+@use 'src/themes/Mikings/variables/fonts';
+@use 'src/themes/Mikings/variables/colors';
+
+@keyframes hide-mini {
+	0% {
+		opacity: 1;
+	}
+	99% {
+		opacity: 0;
+	}
+	100% {
+		display: none;
+	}
+}
+
+.wmde-banner {
+
+	&-full {
+		visibility: hidden;
+		opacity: 0;
+		transform: scale( 1.1 );
+		transition: opacity 500ms globals.$banner-easing, transform 500ms globals.$banner-easing;
+	}
+
+	&-wrapper {
+		font-size: 16px;
+		font-family: fonts.$ui;
+		box-shadow: 0 3px 0.6em rgba( 60 60 60 / 40% );
+		background-color: colors.$white;
+
+		&--full-page {
+			.wmde-banner-mini {
+				animation: hide-mini 500ms;
+			}
+			.wmde-banner-full {
+				visibility: visible;
+				opacity: 1;
+				transform: scale( 1 );
+			}
+		}
+
+		&--soft-closing {
+			.wmde-banner-mini {
+				display: none;
+			}
+		}
+	}
+
+	&--closed {
+		.wmde-banner-wrapper {
+			display: none;
+		}
+	}
+}
+
+</style>
---
+++ banners/wpde_mobile/content/BannerSlides.vue
@@ -0,0 +1,57 @@
+<template>
+	<KeenSlider :interval="5000" :with-navigation="false" :play="play">
+		<template #slides-1>
+			<div class="wmde-banner-slide-content-with-progress-bar">
+				<p>Unser Spendenziel: {{ goalDonationSum }} Millionen Euro</p>
+				<ProgressBar amount-to-show-on-right="TARGET"/>
+			</div>
+		</template>
+		<template #slides-2>
+			<div>
+				<p>An alle, die Wikipedia in Deutschland nutzen:<br/>
+					Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
+					Am heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
+					zu sichern.</p>
+			</div>
+		</template>
+		<template #slides-3>
+			<div>
+				<p>{{ campaignDaySentence }} Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf. Wikipedia
+					wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert.</p>
+			</div>
+		</template>
+		<template #slides-4>
+			<div>
+				<p>Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
+					entwickeln. <span v-if="visitorsVsDonorsSentence !== ''" class="wmde-banner-text-animated-highlight">{{ visitorsVsDonorsSentence }}</span></p>
+			</div>
+		</template>
+		<template #slides-5>
+			<div>
+				<p>Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in
+					diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen Sie sich doch bitte
+						eine Minute Zeit und geben Sie etwas zurück. Vielen Dank!</strong></p>
+			</div>
+		</template>
+	</KeenSlider>
+</template>
+
+<script setup lang="ts">
+import KeenSlider from '@src/components/Slider/KeenSlider.vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import { inject } from 'vue';
+import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
+
+interface Props {
+	play: boolean
+}
+
+defineProps<Props>();
+
+const {
+	currentDayName,
+	campaignDaySentence,
+	visitorsVsDonorsSentence,
+	goalDonationSum
+}: DynamicContent = inject( 'dynamicCampaignText' );
+</script>
