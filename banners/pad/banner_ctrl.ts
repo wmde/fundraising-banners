4f5014d5ca9793aabe816f98f9c27254fd754303
Abban Dunne <abban.dunne@wikimedia.de>
2023-04-26 14:48:27 +0200

    Add iPad banner
    Ticket: https://phabricator.wikimedia.org/T327632

---
+++ banners/pad/banner_ctrl.ts
@@ -0,0 +1,73 @@
+import { createVueApp } from '@src/createVueApp';
+
+import './styles/styles.scss';
+
+import BannerConductor from '@src/components/BannerConductor/BannerConductor.vue';
+import Banner from './components/BannerCtrl.vue';
+import getBannerDelay from '@src/utils/getBannerDelay';
+import { WindowResizeHandler } from '@src/utils/ResizeHandler';
+import PageOrg from '@src/page/PageOrg';
+import { WindowMediaWiki } from '@src/page/MediaWiki/WindowMediaWiki';
+import { SkinFactory } from '@src/page/skin/SkinFactory';
+import { WindowSizeIssueChecker } from '@src/utils/SizeIssueChecker/WindowSizeIssueChecker';
+import TranslationPlugin from '@src/TranslationPlugin';
+
+// Channel specific form setup
+import { createFormItems } from './form_items';
+import { createFormActions } from '@src/createFormActions';
+import { FormController } from './FormController';
+import { useFormModel } from '@src/components/composables/useFormModel';
+
+// Change for EN banners
+import messages from './messages';
+import { Translator } from '@src/Translator';
+import DynamicTextPlugin from '@src/DynamicTextPlugin';
+import { LocalImpressionCount } from '@src/utils/LocalImpressionCount';
+import { Formatters } from '@src/utils/DynamicContent/Formatters';
+import { CurrencyDe } from '@src/utils/DynamicContent/formatters/CurrencyDe';
+import { OrdinalDe } from '@src/utils/DynamicContent/formatters/OrdinalDe';
+import { IntegerDe } from '@src/utils/DynamicContent/formatters/IntegerDe';
+import { DeJSONFundsContentLoader } from '@src/utils/UseOfFunds/DeJSONFundsContentLoader';
+
+const useOfFundsContent = ( new DeJSONFundsContentLoader() ).getContent();
+const translator = new Translator( messages );
+
+// This is channel specific and must be changed for wp.de banners
+const mediaWiki = new WindowMediaWiki();
+const page = new PageOrg( mediaWiki, ( new SkinFactory( mediaWiki ) ).getSkin(), new WindowSizeIssueChecker() );
+
+// This is language-specific and must be changed for EN banners
+const currencyFormatter = new CurrencyDe();
+const formatters: Formatters = { currency: currencyFormatter, ordinal: new OrdinalDe(), integer: new IntegerDe() };
+
+const impressionCount = new LocalImpressionCount( page.getTracking().keyword );
+
+const app = createVueApp( BannerConductor, {
+	page,
+	bannerConfig: {
+		delay: getBannerDelay( 7500 ),
+		transitionDuration: 1000
+	},
+	bannerProps: {
+		formController: new FormController( useFormModel() ),
+		useOfFundsContent
+	},
+	resizeHandler: new WindowResizeHandler(),
+	banner: Banner,
+	impressionCount
+} );
+
+app.use( TranslationPlugin, translator );
+app.use( DynamicTextPlugin, {
+	campaignParameters: page.getCampaignParameters(),
+	date: new Date(),
+	formatters,
+	impressionCount,
+	translator
+} );
+
+app.provide( 'currencyFormatter', currencyFormatter );
+app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
+app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount ) );
+
+app.mount( page.getBannerContainer() );
---
+++ banners/pad/content/BannerSlidesVar.vue
@@ -0,0 +1,51 @@
+<template>
+	<KeenSlider :interval="5000" :with-navigation="true" :play="play">
+		<template #slides-1>
+			<div>
+				<p>
+					<InfoIcon fill="#990a00"/> An alle, die Wikipedia in Deutschland nutzen: Vielleicht kommen wir gerade ungelegen,
+					aber dennoch: Klicken Sie jetzt bitte nicht weg! Am heutigen {{ currentDayName }} bitten wir Sie
+					bescheiden, die Unabhängigkeit von Wikipedia zu sichern.
+				</p>
+			</div>
+		</template>
+		<template #slides-2>
+			<div>
+				<p>{{ campaignDaySentence }} Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf. Wikipedia wird
+					durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert.</p>
+			</div>
+		</template>
+		<template #slides-3>
+			<div>
+				<p>Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
+					entwickeln. ><span class="wmde-banner-text-animated-highlight">{{ visitorsVsDonorsSentence }}</span></p>
+			</div>
+		</template>
+		<template #slides-4>
+			<div>
+				<p>Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem
+					Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute
+					Zeit und geben Sie etwas zurück. Vielen Dank!</p>
+			</div>
+		</template>
+	</KeenSlider>
+</template>
+
+<script setup lang="ts">
+import KeenSlider from '@src/components/Slider/KeenSlider.vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import { inject } from 'vue';
+import InfoIcon from '@src/components/Icons/InfoIcon.vue';
+
+interface Props {
+	play: boolean
+}
+
+defineProps<Props>();
+
+const {
+	currentDayName,
+	campaignDaySentence,
+	visitorsVsDonorsSentence
+}: DynamicContent = inject( 'dynamicCampaignText' );
+</script>
