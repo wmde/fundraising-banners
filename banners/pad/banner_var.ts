42a260865588552159bcfd7482a7f23d9dfb1b13
Abban Dunne <abban.dunne@wikimedia.de>
2023-04-28 11:49:02 +0200

    Convert converters to use locale factory

--- banners/pad/banner_var.ts
+++ banners/pad/banner_var.ts
@@ -23,13 +23,10 @@ import messages from './messages';
 import { Translator } from '@src/Translator';
 import DynamicTextPlugin from '@src/DynamicTextPlugin';
 import { LocalImpressionCount } from '@src/utils/LocalImpressionCount';
-import { Formatters } from '@src/utils/DynamicContent/Formatters';
-import { CurrencyDe } from '@src/utils/DynamicContent/formatters/CurrencyDe';
-import { OrdinalDe } from '@src/utils/DynamicContent/formatters/OrdinalDe';
-import { IntegerDe } from '@src/utils/DynamicContent/formatters/IntegerDe';
-import { DeJSONFundsContentLoader } from '@src/utils/UseOfFunds/DeJSONFundsContentLoader';
+import { LocaleFactoryDe } from '@src/utils/LocaleFactory/LocaleFactoryDe';
 
-const useOfFundsContent = ( new DeJSONFundsContentLoader() ).getContent();
+const localeFactory = new LocaleFactoryDe();
+const useOfFundsContent = localeFactory.getUseOfFundsLoader().getContent();
 const translator = new Translator( messages );
 
 // This is channel specific and must be changed for wp.de banners
@@ -37,8 +34,6 @@ const mediaWiki = new WindowMediaWiki();
 const page = new PageOrg( mediaWiki, ( new SkinFactory( mediaWiki ) ).getSkin(), new WindowSizeIssueChecker() );
 
 // This is language-specific and must be changed for EN banners
-const currencyFormatter = new CurrencyDe();
-const formatters: Formatters = { currency: currencyFormatter, ordinal: new OrdinalDe(), integer: new IntegerDe() };
 
 const impressionCount = new LocalImpressionCount( page.getTracking().keyword );
 
@@ -61,10 +56,12 @@ app.use( TranslationPlugin, translator );
 app.use( DynamicTextPlugin, {
 	campaignParameters: page.getCampaignParameters(),
 	date: new Date(),
-	formatters,
+	formatters: localeFactory.getFormatters(),
 	impressionCount,
 	translator
 } );
+
+const currencyFormatter = localeFactory.getCurrencyFormatter();
 
 app.provide( 'currencyFormatter', currencyFormatter );
 app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
