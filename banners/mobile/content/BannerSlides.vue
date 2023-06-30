5ebb6849014fad942405133325ff338490344673
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-06-30 17:28:19 +0200

    Prepare campaign for C23_WMDE_Mobile_DE_01
    - first pre-campaign test for German Mobile DE
    
    - based on last year's mobile test 14 ctrl
    - changes in slides and text in both ctrl and var
    - progress bar is removed in both banners
    
    https://phabricator.wikimedia.org/T340622

--- banners/mobile/content/BannerSlides.vue
+++ banners/mobile/content/BannerSlides.vue
@@ -1,34 +1,34 @@
 <template>
-	<KeenSliderSlide :is-current="currentSlide === 0" class="wmde-banner-slide-content-with-progress-bar">
-		<p>Unser Spendenziel: {{ goalDonationSum }} Millionen Euro</p>
-		<ProgressBar amount-to-show-on-right="TARGET"/>
+	<KeenSliderSlide :is-current="currentSlide === 0">
+		<p>An alle, die Wikipedia in Deutschland nutzen:</p>
+		<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 1">
-		<p>An alle, die Wikipedia in Deutschland nutzen:<br/>
-			Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
-			Am heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
-			zu sichern.</p>
+		<p>Am heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu unterstützen.</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 2">
-		<p>{{ campaignDaySentence }} Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf. Wikipedia
-			wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert.</p>
+		<p>
+			<span class="wmde-banner-text-animated-highlight">
+				Millionen Menschen nutzen Wikipedia, aber 99 % spenden nicht – sie übergehen diesen Aufruf.
+			</span>
+			Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 3">
-		<p>Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
-			entwickeln. <span v-if="visitorsVsDonorsSentence !== ''"
-				class="wmde-banner-text-animated-highlight">{{ visitorsVsDonorsSentence }}</span></p>
+		<p>Die durchschnittliche Spende beträgt 22,25&nbsp;€, doch bereits eine Spende von 5&nbsp;€ hilft uns
+			weiter. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 4">
-		<p>Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in
-			diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen Sie sich doch bitte
-				eine Minute Zeit und geben Sie etwas zurück. Vielen Dank!</strong></p>
+		<p>
+			<strong>
+				Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!
+			</strong>
+		</p>
 	</KeenSliderSlide>
 </template>
 
 <script setup lang="ts">
 import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
 import { inject } from 'vue';
-import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
 import KeenSliderSlide from '@src/components/Slider/KeenSliderSlide.vue';
 
 interface Props {
