cac89127f75915763968c425aa14c29ceb7b1d79
Sperling-0 <tanujadoriya@gmail.com>
2023-07-18 17:40:05 +0200

    Prepare campaign for C23_WMDE_Desktop_DE_02 CTRL has a text change
    Ticket: https://phabricator.wikimedia.org/T341454

--- banners/desktop/content/BannerText.vue
+++ banners/desktop/content/BannerText.vue
@@ -8,12 +8,11 @@
 			<p>
 				Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
 				heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
-				unterstützen.
-				<span class="wmde-banner-text-animated-highlight">Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden
-			nicht – sie übergehen diesen Aufruf.</span> Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Die durchschnittliche Spende
-				beträgt 22,25&nbsp;€, doch bereits eine Spende von 5&nbsp;€ hilft uns weiter. Hat Wikipedia Ihnen in
-				diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden Sie sich, eine der
-				seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!
+				unterstützen. <span class="wmde-banner-text-animated-highlight">Millionen Menschen nutzen Wikipedia,
+				aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf.</span> Die meisten Menschen spenden, weil sie
+				Wikipedia nützlich finden. Die durchschnittliche Spende beträgt 22,25&nbsp;€, doch bereits 5&nbsp;€ helfen uns
+				weiter. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden
+				Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!
 			</p>
 		</div>
 	</div>
