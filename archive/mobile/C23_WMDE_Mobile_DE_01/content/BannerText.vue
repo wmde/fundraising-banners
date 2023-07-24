73e5d674e3c913d8ac4fdd6d051cfbcbde3db7d6
Sperling-0 <tanujadoriya@gmail.com>
2023-07-24 13:06:31 +0200

    Archive C23_WMDE_Mobile_DE_01
    Ticket: https://phabricator.wikimedia.org/T342204

---
+++ archive/mobile/C23_WMDE_Mobile_DE_01/content/BannerText.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="wmde-banner-message">
+		<div>
+			<p>
+				<strong>An alle, die Wikipedia in Deutschland nutzen: </strong>
+			</p>
+			<p>
+				Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
+				heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
+				unterstützen.
+				<span class="wmde-banner-text-animated-highlight">
+					Millionen Menschen nutzen Wikipedia, aber 99 % spenden nicht – sie übergehen diesen Aufruf.
+				</span>
+				Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Die durchschnittliche Spende
+				beträgt 22,25&nbsp;€, doch bereits eine Spende von 5&nbsp;€ hilft uns weiter. Hat Wikipedia Ihnen in
+				diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann entscheiden Sie sich, eine der
+				seltenen Ausnahmen zu sein, und geben Sie etwas zurück.</strong> Vielen Dank!
+			</p>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+
+const { currentDayName }: DynamicContent = inject( 'dynamicCampaignText' );
+
+</script>
