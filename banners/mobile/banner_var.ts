9097d74ca868f3dcc59ad81521287a77878c6bcc
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-09-13 17:29:10 +0200

    Create VAR for C23_WMDE_Mobile_DE_04
    - form leads to different AB test bucket (address page)
    - change tests
    
    https://phabricator.wikimedia.org/T345530

--- banners/mobile/banner_var.ts
+++ banners/mobile/banner_var.ts
@@ -60,7 +60,7 @@ const currencyFormatter = localeFactory.getCurrencyFormatter();
 
 app.provide( 'currencyFormatter', currencyFormatter );
 app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
-app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount ) );
+app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount, { ast: '2' } ) );
 app.provide( 'tracker', tracker );
 
 app.mount( page.getBannerContainer() );
