7e2511398b89deafe67f390476c686a8fdfbbcb8
Abban Dunne <abban.dunne@wikimedia.de>
2023-10-24 14:00:55 +0200

    Add viewport info to submit tracking

--- banners/english/event_map.ts
+++ banners/english/event_map.ts
@@ -9,6 +9,7 @@ import { NotShownEvent } from '@src/tracking/events/NotShownEvent';
 import { mapNotShownEvent } from '@src/tracking/LegacyEventTracking/mapNotShownEvent';
 import { BannerSubmitEvent } from '@src/tracking/events/BannerSubmitEvent';
 import { WMDESizeIssueEvent } from '@src/tracking/WPORG/WMDEBannerSizeIssue';
+import { createViewportInfo } from '@src/tracking/LegacyEventTracking/createViewportInfo';
 
 export default new Map<string, TrackingEventConverterFactory>( [
 	[ CloseEvent.EVENT_NAME, mapCloseEvent ],
@@ -22,11 +23,11 @@ export default new Map<string, TrackingEventConverterFactory>( [
 	[ BannerSubmitEvent.EVENT_NAME, ( e: BannerSubmitEvent ): WMDESizeIssueEvent => {
 		switch ( e.feature ) {
 			case 'UpgradeToYearlyForm':
-				return new WMDESizeIssueEvent( `submit-${e.userChoice}`, null, 1 );
+				return new WMDESizeIssueEvent( `submit-${e.userChoice}`, createViewportInfo(), 1 );
 			case 'CustomAmountForm':
-				return new WMDESizeIssueEvent( `submit-different-amount`, null, 1 );
+				return new WMDESizeIssueEvent( `submit-different-amount`, createViewportInfo(), 1 );
 			default:
-				return new WMDESizeIssueEvent( `submit`, null, 1 );
+				return new WMDESizeIssueEvent( `submit`, createViewportInfo(), 1 );
 		}
 	} ]
 ] );
