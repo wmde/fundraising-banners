e1e4e5d433d78a92da44194fbc56e16d98030426
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-10-25 17:06:55 +0200

    Archive C23_WMDE_Mobile_DE_05
    https://phabricator.wikimedia.org/T348621

---
+++ archive/mobile/C23_WMDE_Mobile_DE_05/content/BannerText.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="wmde-banner-message">
+		<div>
+			<p class="banner-text-title">
+				<strong>Hi,</strong>
+			</p>
+			<p>
+				vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
+				heutigen {{ currentDayName }} bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
+				unterstützen. <span class="wmde-banner-text-animated-highlight">Millionen Menschen nutzen Wikipedia,
+				aber 99 % spenden nicht – sie übergehen diesen Aufruf.</span>
+				Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Die durchschnittliche Spende
+				beträgt 22,25&nbsp;€, doch bereits 5 € helfen uns weiter. Hat Wikipedia Ihnen in
+				diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann entscheiden Sie sich, eine der
+				seltenen Ausnahmen zu sein, und geben Sie etwas zurück.</strong> Vielen Dank!
+			</p>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+
+const { currentDayName }: DynamicContent = inject( 'dynamicCampaignText' );
+
+</script>
