d9309743791f2296cc4ea9bf6eb8cd705ba271fc
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-02 13:17:53 +0100

    Add tracking events to C23_WPDE_Desktop_01
    This banner was missing some event tracking.
    
    Ticket: https://phabricator.wikimedia.org/T350151

--- banners/wpde_desktop/event_map.ts
+++ banners/wpde_desktop/event_map.ts
@@ -1,8 +1,13 @@
 import { CustomAmountChangedEvent } from '@src/tracking/events/CustomAmountChangedEvent';
 import { CloseEvent } from '@src/tracking/events/CloseEvent';
+import { BannerSubmitEvent } from '@src/tracking/events/BannerSubmitEvent';
+import { FormStepShownEvent } from '@src/tracking/events/FormStepShownEvent';
+import { ShownEvent } from '@src/tracking/events/ShownEvent';
 
 export default new Map( [
 	[ CloseEvent.EVENT_NAME, 0.1 ],
-	[ CustomAmountChangedEvent.EVENT_NAME, 1 ]
-	// TODO add more supported events (e.g. submit). We don't track SizeIssue events on WPDE
+	[ ShownEvent.EVENT_NAME, 1 ],
+	[ FormStepShownEvent.EVENT_NAME, 1 ],
+	[ CustomAmountChangedEvent.EVENT_NAME, 1 ],
+	[ BannerSubmitEvent.EVENT_NAME, 1 ]
 ] );
