a9c276a823aab334d5d3c2ddc6d98e6e4ab57c4f
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-02 13:24:49 +0100

    Add missing event tracking to C23_WPDE_Mobile_01
    This banner was missing some events.
    
    Ticket: https://phabricator.wikimedia.org/T350151

--- banners/wpde_mobile/event_map.ts
+++ banners/wpde_mobile/event_map.ts
@@ -1,8 +1,15 @@
 import { MobileMiniBannerExpandedEvent } from '@src/tracking/events/MobileMiniBannerExpandedEvent';
 import { CloseEvent } from '@src/tracking/events/CloseEvent';
+import { ShownEvent } from '@src/tracking/events/ShownEvent';
+import { FormStepShownEvent } from '@src/tracking/events/FormStepShownEvent';
+import { CustomAmountChangedEvent } from '@src/tracking/events/CustomAmountChangedEvent';
+import { BannerSubmitEvent } from '@src/tracking/events/BannerSubmitEvent';
 
 export default new Map( [
 	[ MobileMiniBannerExpandedEvent.EVENT_NAME, 1 ],
-	[ CloseEvent.EVENT_NAME, 0.1 ]
-	// TODO add more supported events (e.g. submit). We don't track SizeIssue events on WPDE
+	[ CloseEvent.EVENT_NAME, 0.1 ],
+	[ ShownEvent.EVENT_NAME, 1 ],
+	[ FormStepShownEvent.EVENT_NAME, 1 ],
+	[ CustomAmountChangedEvent.EVENT_NAME, 1 ],
+	[ BannerSubmitEvent.EVENT_NAME, 1 ]
 ] );
