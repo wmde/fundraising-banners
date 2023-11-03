1d15392edde3cac150ddc61c5a2b7f514fd2aab1
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-03 10:46:51 +0100

    Archive C23_WMDE_Mobile_DE_06
    Ticket: https://phabricator.wikimedia.org/T350457

---
+++ archive/mobile/C23_WMDE_Mobile_DE_06/components/MiniBanner.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="wmde-banner-mini">
+		<div class="wmde-banner-mini-close">
+			<button class="wmde-banner-mini-close-button t-close-main-banner" @click.prevent="$emit( 'close' )">
+				<CloseIconMobile/>
+			</button>
+		</div>
+
+		<header class="wmde-banner-mini-headline">
+			<div class="wmde-banner-mini-headline-background">
+				<span class="wmde-banner-mini-headline-content">Ist Ihnen Wikipedia 5&nbsp;€ wert?</span>
+			</div>
+		</header>
+
+		<div class="wmde-banner-mini-slideshow">
+			<slot name="banner-slides"/>
+			<slot name="progress"/>
+		</div>
+
+		<button class="wmde-banner-mini-button" @click="$emit( 'showFullPageBanner' )">
+			Jetzt spenden
+		</button>
+
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import CloseIconMobile from '@src/components/Icons/CloseIconMobile.vue';
+
+defineEmits( [ 'showFullPageBanner', 'close' ] );
+
+</script>
---
+++ archive/mobile/C23_WMDE_Mobile_DE_06/content/BannerSlides.vue
@@ -0,0 +1,67 @@
+<template>
+
+	<KeenSliderSlide :is-current="currentSlide === 0" class="wmde-banner-slide-content-with-progress-bar">
+		<p>Unser Spendenziel: {{ goalDonationSum }} Millionen</p>
+		<ProgressBar amount-to-show-on-right="TARGET"/>
+	</KeenSliderSlide>
+
+	<KeenSliderSlide :is-current="currentSlide === 1">
+		<p>
+			<strong>Hi,</strong>
+		</p>
+		<p>vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
+			heutigen {{ currentDayName }}, den {{ currentDate }}, bitten wir Sie bescheiden, die Unabhängigkeit
+			von Wikipedia zu unterstützen.</p>
+	</KeenSliderSlide>
+
+	<KeenSliderSlide :is-current="currentSlide === 2">
+		<p>
+			Insgesamt spenden 99% nichts - sie übergehen diesen Aufruf. Die durchschnittliche Spende
+			beträgt 22,25&nbsp;€, doch bereits ein kleiner Beitrag hilft uns weiter.
+		</p>
+	</KeenSliderSlide>
+
+	<KeenSliderSlide :is-current="currentSlide === 3">
+		<p>
+			<span class="wmde-banner-text-animated-highlight">
+				{{ visitorsVsDonorsSentence }}
+			</span>
+			Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
+		</p>
+	</KeenSliderSlide>
+
+	<KeenSliderSlide :is-current="currentSlide === 4">
+		<p>
+			Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden
+			Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!
+		</p>
+	</KeenSliderSlide>
+
+</template>
+
+<script setup lang="ts">
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import { computed, inject } from 'vue';
+import KeenSliderSlide from '@src/components/Slider/KeenSliderSlide.vue';
+import ProgressBar from '@src/components/ProgressBar/ProgressBar.vue';
+
+interface Props {
+	currentSlide: number
+}
+
+defineProps<Props>();
+
+const {
+	currentDayName,
+	goalDonationSum,
+	visitorsVsDonorsSentence
+}: DynamicContent = inject( 'dynamicCampaignText' );
+
+const currentDate = computed( () => {
+	return new Intl.DateTimeFormat( 'de-DE', {
+		day: '2-digit',
+		month: 'long'
+	} ).format( new Date() );
+} );
+
+</script>
