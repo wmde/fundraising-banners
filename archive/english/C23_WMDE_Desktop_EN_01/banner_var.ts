e4ec09ae36ceaccfaee09a4f9ca5d7ae2d298c79
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-06 15:12:21 +0100

    Archive C23_WMDE_Desktop_EN_01
    Ticket: https://phabricator.wikimedia.org/T350573

---
+++ archive/english/C23_WMDE_Desktop_EN_01/banner_var.ts
@@ -0,0 +1,68 @@
+import { createVueApp } from '@src/createVueApp';
+
+import './styles/styles_var.scss';
+
+import BannerConductor from '@src/components/BannerConductor/BannerConductor.vue';
+import Banner from './components/BannerVar.vue';
+import getBannerDelay from '@src/utils/getBannerDelay';
+import { WindowResizeHandler } from '@src/utils/ResizeHandler';
+import PageWPORG from '@src/page/PageWPORG';
+import { WindowMediaWiki } from '@src/page/MediaWiki/WindowMediaWiki';
+import { SkinFactory } from '@src/page/skin/SkinFactory';
+import { WindowSizeIssueChecker } from '@src/utils/SizeIssueChecker/WindowSizeIssueChecker';
+import TranslationPlugin from '@src/TranslationPlugin';
+import { Translator } from '@src/Translator';
+import DynamicTextPlugin from '@src/DynamicTextPlugin';
+import { LocalImpressionCount } from '@src/utils/LocalImpressionCount';
+import { LegacyTrackerWPORG } from '@src/tracking/LegacyTrackerWPORG';
+import { Locales } from '@src/domain/Locales';
+
+// Locale-specific imports
+import messages from './messages';
+import { LocaleFactoryEn } from '@src/utils/LocaleFactory/LocaleFactoryEn';
+
+// Channel specific form setup
+import { createFormItems } from './form_items';
+import { createFormActions } from '@src/createFormActions';
+import eventMappings from './event_map';
+
+const localeFactory = new LocaleFactoryEn();
+const translator = new Translator( messages );
+const mediaWiki = new WindowMediaWiki();
+const page = new PageWPORG( mediaWiki, ( new SkinFactory( mediaWiki ) ).getSkin(), new WindowSizeIssueChecker( 800 ) );
+const impressionCount = new LocalImpressionCount( page.getTracking().keyword );
+const tracker = new LegacyTrackerWPORG( mediaWiki, page.getTracking().keyword, eventMappings );
+const remainingImpressions = Math.max( page.getMaxBannerImpressions( 'english' ) - impressionCount.overallCountIncremented, 0 );
+
+const app = createVueApp( BannerConductor, {
+	page,
+	bannerConfig: {
+		delay: getBannerDelay( 7500 ),
+		transitionDuration: 1000
+	},
+	bannerProps: {
+		useOfFundsContent: localeFactory.getUseOfFundsLoader().getContent(),
+		remainingImpressions
+	},
+	resizeHandler: new WindowResizeHandler(),
+	banner: Banner,
+	impressionCount
+} );
+
+app.use( TranslationPlugin, translator );
+app.use( DynamicTextPlugin, {
+	campaignParameters: page.getCampaignParameters(),
+	date: new Date(),
+	formatters: localeFactory.getFormatters(),
+	impressionCount,
+	translator
+} );
+
+const currencyFormatter = localeFactory.getCurrencyFormatter();
+
+app.provide( 'currencyFormatter', currencyFormatter );
+app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
+app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount, { locale: Locales.EN } ) );
+app.provide( 'tracker', tracker );
+
+app.mount( page.getBannerContainer() );
---
+++ archive/english/C23_WMDE_Desktop_EN_01/content/BannerSlides.vue
@@ -0,0 +1,42 @@
+<template>
+	<KeenSliderSlide :is-current="currentSlide === 0">
+		<p class="headline">
+			<InfoIcon fill="#990a00"/>
+			<strong> To all our readers in Germany,</strong>
+		</p>
+		<p>
+			This {{ currentDayName }}, {{ currentDate }}, with only a few weeks before 2023 comes to an end, we humbly
+			ask for your support. We are the nonprofit that collects donations on Wikipedia to support free knowledge.
+			If you can, please join the 1% of readers who give.
+		</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide :is-current="currentSlide === 1">
+		<p>
+			<span class="wmde-banner-text-animated-highlight">If everyone reading this right now gave just €5, we'd hit
+			our annual goal in a couple of hours.</span> The price of a cup of coffee is all we ask. It's easy to ignore
+			our messages, but we hope you'll take one minute to think about how useful Wikipedia is in your life.
+		</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide :is-current="currentSlide === 2">
+		<p>
+			Online, Wikipedia is the closest thing we have to a public park or a library where everyone can go to learn.
+			If Wikipedia has given you €5 worth of knowledge this year, please support the technology that makes our
+			projects possible and advance the cause of free knowledge worldwide. — <i>Wikimedia Deutschland</i>
+		</p>
+	</KeenSliderSlide>
+</template>
+
+<script setup lang="ts">
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import { inject } from 'vue';
+import InfoIcon from '@src/components/Icons/InfoIcon.vue';
+import KeenSliderSlide from '@src/components/Slider/KeenSliderSlide.vue';
+
+interface Props {
+	currentSlide: number
+}
+
+defineProps<Props>();
+
+const { currentDayName, currentDate }: DynamicContent = inject( 'dynamicCampaignText' );
+</script>
