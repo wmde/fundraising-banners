550604574f96652c6f0f0497c992f94759e74893
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-07 09:47:47 +0100

    Update copy for C23_WMDE_Desktop_EN_02_2
    C23_WMDE_Desktop_EN_02 had incorrect copy in the slider.
    
    Ticket: https://phabricator.wikimedia.org/T350660

--- banners/english/content/BannerText.vue
+++ banners/english/content/BannerText.vue
@@ -9,8 +9,7 @@
 				Please don't ignore this 1-minute read. This {{ currentDayName }}, {{ currentDate }}, I ask you to
 				reflect on the number of times you visited Wikipedia in the past year, the value you got from it,
 				and whether you're able to give 5€ back. If you can, please join the 1% of readers who
-				give. <span class="wmde-banner-text-animated-highlight">If everyone reading this right now gave
-				just 5€, we'd hit our goal in a couple of hours.</span>
+				give. <AnimatedText content="If everyone reading this right now gave just 5€, we'd hit our goal in a couple of hours."/>
 			</p>
 
 			<p>
@@ -33,6 +32,7 @@
 import { inject } from 'vue';
 import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
 import InfoIcon from '@src/components/Icons/InfoIcon.vue';
+import AnimatedText from '@src/components/AnimatedText/AnimatedText.vue';
 
 const { currentDayName, currentDate }: DynamicContent = inject( 'dynamicCampaignText' );
 
