97abdfe34c6c57c6941c30ed81c2915efc6f5f60
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-16 07:31:26 +0100

    Decrease WPDE banner limit from 10 to 7
    Ticket: https://phabricator.wikimedia.org/T349843

--- src/page/PageWPDE.ts
+++ src/page/PageWPDE.ts
@@ -81,9 +81,8 @@ class PageWPDE implements Page {
 	}
 
 	public getMaxBannerImpressions(): number {
-		// WPDE banners have a hardcoded limit of 10
-		// TODO: Change this to 7 when this ticket is completed https://phabricator.wikimedia.org/T349843
-		return 10;
+		// WPDE banners have a hardcoded limit of 7
+		return 7;
 	}
 }
 
