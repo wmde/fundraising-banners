ba0f56bdb145c7d453980a30b0472a076ed6a0bf
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-30 08:50:01 +0100

    Decrease page gap for Vector2022
    One of the rules for showing a banner is that the Wikipedia
    logo must be visible on a person's screen when the banner is
    loaded. But on Vector2022 the logo is much smaller than on
    Vector meaning we can decrease this gap and reduce size issues.

--- src/page/skin/Vector2022.ts
+++ src/page/skin/Vector2022.ts
@@ -1,6 +1,9 @@
 import Vector from '@src/page/skin/Vector';
 
 class Vector2022 extends Vector {
+	public minimumVisiblePageBeneathBanner(): number {
+		return 72;
+	}
 }
 
 export default Vector2022;
