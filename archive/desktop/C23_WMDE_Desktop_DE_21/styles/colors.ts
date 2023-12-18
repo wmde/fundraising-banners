e051bdc1c3a4a118bcbddb1e729b284c0ee73cfe
Abban Dunne <abban.dunne@wikimedia.de>
2023-12-18 15:24:47 +0100

    Archive C23_WMDE_Desktop_DE_21
    Ticket: https://phabricator.wikimedia.org/T353631

---
+++ archive/desktop/C23_WMDE_Desktop_DE_21/styles/colors.ts
@@ -0,0 +1,27 @@
+export default {
+	// "Primary" accent color, mostly for borders and progress bar fill
+	'--color-primary': '#990a00',
+	'--color-primary-light': '#dbb0ad',
+
+	// "Secondary" color, for backgrounds
+	'--color-secondary': '#2a4b8d',
+	'--color-secondary-light': '#4465a7',
+
+	'--color-error': '#990000',
+
+	'--color-white': '#ffffff',
+	'--color-black': '#000000',
+	'--color-black-light': '#202122',
+	'--color-gray': '#808080',
+	'--color-gray-medium': '#747474',
+	'--color-gray-light': '#dddddd',
+
+	'--animated-highlight-gradient': `
+	linear-gradient(
+		270deg,
+		rgba( 255, 255, 255, 0 ) 0%,
+		rgba( 255, 255, 255, 0 ) 50%,
+		rgba(  153, 10, 0 ) 50%,
+		rgba(  153, 10, 0 ) 100%
+	)`
+};
