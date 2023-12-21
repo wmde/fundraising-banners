f0a235fd266afb011db916cb1c93ebbd47e1ffdd
Abban Dunne <abban.dunne@wikimedia.de>
2023-12-21 09:06:42 +0100

    Prepare campaign for C23_WMDE_Desktop_DE_25
    Based on the VAR of test 24
    
    Ticket: https://phabricator.wikimedia.org/T353639

--- banners/desktop/content/BannerSlides.vue
+++ banners/desktop/content/BannerSlides.vue
@@ -1,29 +1,31 @@
 <template>
 	<KeenSliderSlide :is-current="currentSlide === 0">
 		<p>
-			<InfoIcon fill="#990a00"/> <strong>{{ currentDate }}, {{ currentTime }} - An alle, die Wikipedia in Deutschland nutzen. Die Zeit wird knapp!</strong>
+			<InfoIcon fill="#990a00"/> {{ currentDate }}, {{ currentTime }} - An alle, die Wikipedia in Deutschland
+			nutzen. Die Zeit wird knapp! Noch fehlen {{ remainingDonationSum }}.
 		</p>
 		<p>
-			Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
-			heutigen {{ currentDayName }}, den {{ currentDate }}, bitten wir Sie, die Unabhängigkeit
-			von Wikipedia zu unterstützen.
+			Verzeihen Sie die Störung, aber es ist dringend. {{ campaignDaySentence }}
 		</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 1">
 		<p>
-			{{ campaignDaySentence }} <AnimatedText :content="visitorsVsDonorsSentence"/>
+			Daher bitten wir Sie am heutigen {{ currentDayName }}, den {{ currentDate }}, die Unabhängigkeit von
+			Wikipedia zu unterstützen. <AnimatedText :content="visitorsVsDonorsSentence"/>
 		</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 2">
 		<p>
-			Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Die durchschnittliche Spende
-			beträgt 22,25&nbsp;€, doch bereits 5&nbsp;€ helfen uns weiter.
+			Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Die meisten Menschen spenden, weil sie
+			Wikipedia nützlich finden. Die durchschnittliche Spende beträgt 22,25&nbsp;€, doch bereits 5&nbsp;€ helfen
+			uns weiter.
 		</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 3">
 		<p>
-			Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden
-			Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück. Vielen Dank!
+			Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann ist jetzt der
+			Zeitpunkt, eine der seltenen Ausnahmen zu sein und etwas zurückzugeben. Helfen Sie uns, unser Spendenziel
+			zu erreichen. Vielen Dank!
 		</p>
 	</KeenSliderSlide>
 </template>
@@ -47,6 +49,7 @@ const {
 	currentDate,
 	getCurrentTime,
 	campaignDaySentence,
+	remainingDonationSum,
 	visitorsVsDonorsSentence
 } = inject<DynamicContent>( 'dynamicCampaignText' );
 
