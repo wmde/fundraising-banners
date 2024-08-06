7bea5997eb5de9151d716e1eb3bc91501b9f7a18
Sperling-0 <tanujadoriya@gmail.com>
2024-08-06 15:04:07 +0200

    Implement VAR for C24_WMDE_Desktop_DE_03
    - VAR has different donation form styles inspired from English desktop banner
    
    Ticket: https://phabricator.wikimedia.org/T371476

--- banners/desktop/C24_WMDE_Desktop_DE_03/banner_var.ts
+++ banners/desktop/C24_WMDE_Desktop_DE_03/banner_var.ts
@@ -1,6 +1,6 @@
 import { createVueApp } from '@src/createVueApp';
 
-import './styles/styles.scss';
+import './styles/stylesVar.scss';
 
 import BannerConductor from '@src/components/BannerConductor/FallbackBannerConductor.vue';
 import Banner from './components/BannerCtrl.vue';
