ae9651e39c0787da9358ac8b326207625d666217
Abban Dunne <abban.dunne@wikimedia.de>
2024-09-03 08:47:17 +0200

    Improve Treedip dark styles
    - Adjust dark mode colours
    - Fix up form radio button styles
    - Share radio styles between radio fields and custom amount
    - Keep the box shadow on light mode
    - Improve fallback banner dark styles
    
    Ticket: https://phabricator.wikimedia.org/T372553

--- banners/english/C24_WMDE_Desktop_EN_01/content/FallbackSlides.vue
+++ banners/english/C24_WMDE_Desktop_EN_01/content/FallbackSlides.vue
@@ -1,20 +1,15 @@
 <template>
 	<KeenSliderSlide :is-current="currentSlide === 0">
 		<p>
-			Our donation target: €9.7 million
+			If Wikipedia has given you €5 worth of knowledge this year, please give back.
 		</p>
 	</KeenSliderSlide>
 	<KeenSliderSlide :is-current="currentSlide === 1">
 		<p>
-			If Wikipedia has given you €5 worth of knowledge this year, please give back.
-		</p>
-	</KeenSliderSlide>
-	<KeenSliderSlide :is-current="currentSlide === 2">
-		<p>
 			If you can, please join the 1% of readers who give. <em>Thank you.</em>
 		</p>
 	</KeenSliderSlide>
-	<KeenSliderSlide :is-current="currentSlide === 3">
+	<KeenSliderSlide :is-current="currentSlide === 2">
 		<FallbackBank/>
 	</KeenSliderSlide>
 </template>
