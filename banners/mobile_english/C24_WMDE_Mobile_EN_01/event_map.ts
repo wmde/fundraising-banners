c535ab6adcf8be162dd941590e01e21202e5ffa1
Abban Dunne <abban.dunne@wikimedia.de>
2024-10-15 15:50:09 +0200

    Prepare CTRL for C24_WMDE_Mobile_EN_01
    Based on ctrl of C23_WMDE_Mobile_EN_02
    
    - Add styles for the upgrade to yearly form
    - Add event tracking for soft close already donated
    
    Ticket: https://phabricator.wikimedia.org/T376794

--- banners/mobile_english/C24_WMDE_Mobile_EN_01/event_map.ts
+++ banners/mobile_english/C24_WMDE_Mobile_EN_01/event_map.ts
@@ -1,6 +1,8 @@
 import { TrackingEventConverterFactory } from '@src/tracking/LegacyTrackerWPORG';
 import { WMDELegacyBannerEvent } from '@src/tracking/WPORG/WMDELegacyBannerEvent';
 import { MobileMiniBannerExpandedEvent } from '@src/tracking/events/MobileMiniBannerExpandedEvent';
+import { FormStepShownEvent } from '@src/tracking/events/FormStepShownEvent';
+import { mapFormStepShownEvent } from '@src/tracking/LegacyEventTracking/mapFormStepShownEvent';
 import { CloseEvent } from '@src/tracking/events/CloseEvent';
 import { mapCloseEvent } from '@src/tracking/LegacyEventTracking/mapCloseEvent';
 import { NotShownEvent } from '@src/tracking/events/NotShownEvent';
@@ -11,8 +13,18 @@ import { createViewportInfo } from '@src/tracking/LegacyEventTracking/createView
 
 export default new Map<string, TrackingEventConverterFactory>( [
 	[ CloseEvent.EVENT_NAME, mapCloseEvent ],
-	[ NotShownEvent.EVENT_NAME, mapNotShownEvent ],
 	[ MobileMiniBannerExpandedEvent.EVENT_NAME,
 		( e: MobileMiniBannerExpandedEvent ): WMDELegacyBannerEvent => new WMDELegacyBannerEvent( e.eventName + ( e.userChoice !== '' ? `-${e.userChoice}` : '' ), 1 ) ],
-	[ BannerSubmitEvent.EVENT_NAME, (): WMDESizeIssueEvent => new WMDESizeIssueEvent( `submit`, createViewportInfo(), 1 ) ]
+	[ FormStepShownEvent.EVENT_NAME, mapFormStepShownEvent ],
+	[ NotShownEvent.EVENT_NAME, mapNotShownEvent ],
+
+	[ BannerSubmitEvent.EVENT_NAME, ( e: BannerSubmitEvent ): WMDESizeIssueEvent => {
+		switch ( e.feature ) {
+			case 'MiniBanner':
+			case 'UpgradeToYearlyForm':
+				return new WMDESizeIssueEvent( `submit-${e.userChoice}`, createViewportInfo(), 1 );
+			default:
+				return new WMDESizeIssueEvent( `submit`, createViewportInfo(), 1 );
+		}
+	} ]
 ] );
