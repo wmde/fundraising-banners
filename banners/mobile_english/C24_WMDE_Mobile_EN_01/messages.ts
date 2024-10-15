08529c42b83878a4fd8d17f669442d747e6166fa
Abban Dunne <abban.dunne@wikimedia.de>
2024-10-15 15:50:09 +0200

    Prepare CTRL for C24_WMDE_Mobile_EN_01
    Based on ctrl of C23_WMDE_Mobile_EN_02
    
    - Add styles for the upgrade to yearly form
    - Add event tracking for soft close already donated
    
    Ticket: https://phabricator.wikimedia.org/T376794

--- banners/mobile_english/C24_WMDE_Mobile_EN_01/messages.ts
+++ banners/mobile_english/C24_WMDE_Mobile_EN_01/messages.ts
@@ -4,13 +4,16 @@ import { TranslationMessages } from '@src/Translator';
 import SoftClose from '@src/components/SoftClose/messages/SoftClose.en';
 import Footer from '@src/components/Footer/messages/Footer.en';
 import MainDonationForm from '@src/components/DonationForm/Forms/messages/MainDonationForm.en';
+import UpgradeToYearly from '@src/components/DonationForm/Forms/messages/UpgradeToYearly.en';
 
 const messages: TranslationMessages = {
 	...CustomAmountForm,
 	...DynamicCampaignText,
 	...SoftClose,
 	...Footer,
-	...MainDonationForm
+	...MainDonationForm,
+	...UpgradeToYearly,
+	'soft-close-button-already-donated': 'I already donated'
 };
 
 export default messages;
