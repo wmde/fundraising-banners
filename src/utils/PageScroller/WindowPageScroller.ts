0bc00a8a3de9cff6e4eab608b0eb63e7fbdebf34
Abban Dunne <abban.dunne@wikimedia.de>
2024-10-25 18:19:41 +0200

    Make page scroller scroll to top of element
    We use the page scroller to show the donation form
    but this doesn't always align to the top. This modifies
    the behavior to make that happen.

--- src/utils/PageScroller/WindowPageScroller.ts
+++ src/utils/PageScroller/WindowPageScroller.ts
@@ -3,7 +3,7 @@ import { PageScroller } from '@src/utils/PageScroller/PageScroller';
 export class WindowPageScroller implements PageScroller {
 	public scrollIntoView( classSelector: string ): void {
 		document.querySelector( classSelector )?.scrollIntoView(
-			{ behavior: 'smooth', block: 'center', inline: 'nearest' }
+			{ behavior: 'smooth', block: 'start', inline: 'start' }
 		);
 	}
 
