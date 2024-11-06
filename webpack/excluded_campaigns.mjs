46be4a02fb5b2a84dee8df5e4229709e99a7c185
Gabriel Birke <gabriel.birke@wikimedia.de>
2024-11-06 19:27:11 +0100

    Only lint CSS of active campaigns
    Our StyleLint configuration was linting the growing `banners/`
    directory. This commit helps to exclude all non-active banners, shaving
    off about 5 seconds from the dev environment and linting task.

---
+++ webpack/excluded_campaigns.mjs
@@ -0,0 +1,29 @@
+import fg from 'fast-glob';
+import path from 'path';
+import { parse as parseTOML } from "toml";
+import { readFileSync } from "fs";
+
+/**
+ * Get a list of campaign directories in 'banners/' that are not referenced in the current campaign_info.toml
+ *
+ * @param {string} campaignConfigPath
+ * @returns string[]
+ */
+export function getExcludedCampaignDirectories( campaignConfigPath ) {
+	const config = parseTOML( readFileSync( campaignConfigPath, 'utf-8' ) );
+	const activeCampaigns = new Set();
+	for ( const channelConfig of Object.values( config) ) {
+		activeCampaigns.add( channelConfig.campaign );
+	}
+
+	const bannerDirectories = fg.sync( 'banners/*/*', { onlyDirectories: true } );
+	const excludedDirectories = [];
+	for ( const currentDirectory of bannerDirectories ) {
+		const campaignName = path.basename( currentDirectory );
+		if ( activeCampaigns.has( campaignName ) || campaignName.includes( 'thankyou' ) ) {
+			continue;
+		}
+		excludedDirectories.push( `${currentDirectory}/*` );
+	}
+	return excludedDirectories;
+}
