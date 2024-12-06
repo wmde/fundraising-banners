4ca0d3a2c2256993d231fc72da6543e04138e32d
Sperling-0 <tanuja.doriya@wikimedia.de>
2024-12-06 21:58:25 +0100

    Implement VAR for C24_WMDE_Desktop_DE_19
    - Emit the event `modalOpened` when the use of funds modal is opened.
    - Emit the event `modalClosed` when the use of funds modal is closed.
    - The variant banner design is changed according to the design file for the slider and the non-slider.
    - The variant banner integrates the "10 good reasons" feature using the sticker.
    - Diverging from the design, the additional element should be at the bottom (sharing the space with the slide indicators) to prevent overlapping.
    - Hovering over the new element:
       - turns the cursor into a pointer.
       - adds underlining to the link text.
    - Interaction with `ReasonsToDonate` is tracked
    - Add tests for VAR banner
    
    Ticket: https://phabricator.wikimedia.org/T380852

--- test/features/ReasonsToDonate.ts
+++ test/features/ReasonsToDonate.ts
@@ -19,6 +19,14 @@ const expectScrollsToFormWhenCallToActionIsClicked = async ( wrapper: VueWrapper
 	expect( pageScroller.scrollIntoView ).toHaveBeenCalledWith( '.wmde-banner-form' );
 };
 
+const expectScrollsToFormWhenCallToActionIsClickedUsingTenGoodReasonsSticker = async ( wrapper: VueWrapper<any>, pageScroller: PageScroller ): Promise<any> => {
+	await wrapper.find( '.ten-good-reasons-sticker-text' ).trigger( 'click' );
+	await wrapper.find( '.wmde-banner-10-reasons-cta button' ).trigger( 'click' );
+
+	expect( pageScroller.scrollIntoView ).toHaveBeenCalledOnce();
+	expect( pageScroller.scrollIntoView ).toHaveBeenCalledWith( '.wmde-banner-form' );
+};
+
 const expectTracksReasonsToDonateShownEvent = async ( wrapper: VueWrapper<any>, tracker: Tracker ): Promise<any> => {
 
 	await wrapper.find( '.wmde-banner-reasons-to-donate-link' ).trigger( 'click' );
@@ -27,8 +35,25 @@ const expectTracksReasonsToDonateShownEvent = async ( wrapper: VueWrapper<any>, 
 	expect( tracker.trackEvent ).toHaveBeenCalledWith( new ReasonsToDonateShownEvent() );
 };
 
+const expectTracksReasonsToDonateShownEventUsingTenGoodReasonsSticker = async ( wrapper: VueWrapper<any>, tracker: Tracker ): Promise<any> => {
+
+	await wrapper.find( '.ten-good-reasons-sticker-text' ).trigger( 'click' );
+
+	expect( tracker.trackEvent ).toHaveBeenCalledOnce();
+	expect( tracker.trackEvent ).toHaveBeenCalledWith( new ReasonsToDonateShownEvent() );
+};
+
 const expectTracksReasonsToDonateCTAClickedEvent = async ( wrapper: VueWrapper<any>, tracker: Tracker ): Promise<any> => {
 	await wrapper.find( '.wmde-banner-reasons-to-donate-link' ).trigger( 'click' );
+
+	await wrapper.find( '.wmde-banner-10-reasons-cta button' ).trigger( 'click' );
+
+	expect( tracker.trackEvent ).toHaveBeenCalledTimes( 2 );
+	expect( tracker.trackEvent ).toBeCalledWith( new ReasonsToDonateCTAClickedEvent() );
+};
+
+const expectTracksReasonsToDonateCTAClickedEventUsingTenGoodReasonsSticker = async ( wrapper: VueWrapper<any>, tracker: Tracker ): Promise<any> => {
+	await wrapper.find( '.ten-good-reasons-sticker-text' ).trigger( 'click' );
 
 	await wrapper.find( '.wmde-banner-10-reasons-cta button' ).trigger( 'click' );
 
@@ -45,14 +70,33 @@ const expectTracksReasonsToDonateItemClickedEvent = async ( wrapper: VueWrapper<
 	expect( tracker.trackEvent ).toHaveBeenCalledWith( new ReasonsToDonateItemClickedEvent( itemNumber ) );
 };
 
+const expectTracksReasonsToDonateItemClickedEventUsingTenGoodReasonsSticker = async ( wrapper: VueWrapper<any>, tracker: Tracker ): Promise<any> => {
+	await wrapper.find( '.ten-good-reasons-sticker-text' ).trigger( 'click' );
+
+	const itemNumber = '5';
+	await wrapper.find( `.wmde-banner-10-reasons-accordion-item:nth-child(${itemNumber}) .wmde-banner-10-reasons-accordion-title` ).trigger( 'click' );
+
+	expect( tracker.trackEvent ).toHaveBeenCalledWith( new ReasonsToDonateItemClickedEvent( itemNumber ) );
+};
+
 export const useReasonsToDonateFeatures: Record<string, ( wrapper: VueWrapper<any>, tracker: Tracker ) => Promise<any>> = {
 	expectContainsReasonsToDonateDialogue,
 	expectTracksReasonsToDonateShownEvent,
 	expectTracksReasonsToDonateCTAClickedEvent,
 	expectTracksReasonsToDonateItemClickedEvent
+};
 
+export const useReasonsToDonateFeaturesUsingTenGoodReasonsSticker: Record<string, ( wrapper: VueWrapper<any>, tracker: Tracker ) => Promise<any>> = {
+	expectContainsReasonsToDonateDialogue,
+	expectTracksReasonsToDonateShownEventUsingTenGoodReasonsSticker,
+	expectTracksReasonsToDonateCTAClickedEventUsingTenGoodReasonsSticker,
+	expectTracksReasonsToDonateItemClickedEventUsingTenGoodReasonsSticker
 };
 
 export const useReasonsToDonateScrollFeatures: Record<string, ( wrapper: VueWrapper<any>, pageScroller: PageScroller ) => Promise<any>> = {
 	expectScrollsToFormWhenCallToActionIsClicked
+};
+
+export const useReasonsToDonateScrollFeaturesUsingTenGoodReasonsSticker: Record<string, ( wrapper: VueWrapper<any>, pageScroller: PageScroller ) => Promise<any>> = {
+	expectScrollsToFormWhenCallToActionIsClickedUsingTenGoodReasonsSticker
 };
