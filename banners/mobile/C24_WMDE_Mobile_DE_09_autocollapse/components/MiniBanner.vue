0809499c1b42377a7881ea41a98dd9f98668be53
Abban Dunne <abban.dunne@wikimedia.de>
2024-12-11 14:30:27 +0100

    Add example of mobile auto minimise
    Ticket: https://phabricator.wikimedia.org/T380464

---
+++ banners/mobile/C24_WMDE_Mobile_DE_09_autocollapse/components/MiniBanner.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="wmde-banner-mini">
+		<div class="wmde-banner-mini-inner">
+			<div class="wmde-banner-mini-close">
+				<button class="wmde-banner-mini-close-button t-close-main-banner" @click.prevent="$emit( 'close' )">
+					<CloseIconMobile/>
+				</button>
+			</div>
+
+			<header class="wmde-banner-mini-headline">
+				<div class="wmde-banner-mini-headline-background">
+					<span class="wmde-banner-mini-headline-content">Wikipedia ist unverkäuflich</span>
+				</div>
+			</header>
+
+			<div class="wmde-banner-mini-slideshow">
+				<slot name="banner-slides"/>
+			</div>
+
+			<div class="wmde-banner-mini-button-group">
+				<button class="wmde-banner-mini-button-preselect" @click="$emit( 'showFullPageBannerPreselected' )">
+					Jetzt 10 &euro; spenden
+				</button>
+				<button class="wmde-banner-mini-button" @click="$emit( 'showFullPageBanner' )">
+					Anderen Betrag
+				</button>
+			</div>
+		</div>
+		<div class="wmde-banner-mini-minimise">
+			<div class="wmde-banner-mini-minimise-tab">
+				<button class="wmde-banner-mini-minimise-button" @click="$emit( 'minimise' )">
+					Banner verkleinern <MinimiseIcon/>
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import CloseIconMobile from '@src/components/Icons/CloseIconMobile.vue';
+import MinimiseIcon from '@src/components/Icons/MinimiseIcon.vue';
+
+defineEmits( [ 'showFullPageBanner', 'showFullPageBannerPreselected', 'close', 'minimise' ] );
+
+</script>
