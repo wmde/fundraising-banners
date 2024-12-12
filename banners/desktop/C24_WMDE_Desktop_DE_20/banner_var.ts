d791a79702acef5d7776dd468ba4068b24ca6ce8
Sperling-0 <tanuja.doriya@wikimedia.de>
2024-12-12 15:18:36 +0100

    Prepare Var for C24_WMDE_Desktop_DE_20
    Ticket: https://phabricator.wikimedia.org/T381401

--- banners/desktop/C24_WMDE_Desktop_DE_20/banner_var.ts
+++ banners/desktop/C24_WMDE_Desktop_DE_20/banner_var.ts
@@ -1,10 +1,10 @@
 import { createVueApp } from '@src/createVueApp';
 
-import './styles/styles.scss';
+import './styles/styles_var.scss';
 
 import BannerConductor from '@src/components/BannerConductor/FallbackBannerConductor.vue';
-import Banner from './components/BannerCtrl.vue';
-import FallbackBanner from './components/FallbackBanner.vue';
+import Banner from './components/BannerVar.vue';
+import FallbackBanner from './components/FallbackBannerVar.vue';
 import { UrlRuntimeEnvironment } from '@src/utils/RuntimeEnvironment';
 import { WindowResizeHandler } from '@src/utils/ResizeHandler';
 import PageWPORG from '@src/page/PageWPORG';
@@ -19,7 +19,7 @@ import { LegacyTrackerWPORG } from '@src/tracking/LegacyTrackerWPORG';
 import eventMappings from './event_map';
 import { createFallbackDonationURL } from '@src/createFallbackDonationURL';
 import { LocalStorageCloseTracker } from '@src/utils/LocalCloseTracker';
-import messages from './messages';
+import messages from './messages_var';
 import { LocaleFactoryDe } from '@src/utils/LocaleFactory/LocaleFactoryDe';
 import { createFormItems } from './form_items';
 import { createFormActions } from '@src/createFormActions';
