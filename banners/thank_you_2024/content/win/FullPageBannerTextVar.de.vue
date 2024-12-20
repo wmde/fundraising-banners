1ff4a1140f7f135780c40147a088a9bdf8e17009
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2024-12-20 18:16:33 +0100

    Fix height, text highlighting, button text

--- banners/thank_you_2024/content/win/FullPageBannerTextVar.de.vue
+++ banners/thank_you_2024/content/win/FullPageBannerTextVar.de.vue
@@ -3,9 +3,12 @@
 
 	<div class="wmde-banner-content-columns">
 		<p>
-			Denn es gibt viel zu tun: Der tiefgreifende technologische Umbruch, den wir in Zeiten Künstlicher Intelligenz sowie Fakenews und Manipulation im digitalen Raum
-			erleben, macht unser technisches Engagement für Wikipedia wichtiger denn je. Auch in diesem Jahr investieren wir den größten Teil unseres Budgets in die technische
-			Weiterentwicklung unserer Projekte. Damit stellen wir sicher, dass wir von der technischen Entwicklung nicht überrollt werden, sondern sie mitgestalten.
+			Denn es gibt viel zu tun:
+			Der tiefgreifende technologische Umbruch,
+			den wir in Zeiten Künstlicher Intelligenz sowie Fakenews und Manipulation im digitalen Raum erleben,
+			macht unser technisches Engagement für Wikipedia wichtiger denn je.
+			Auch in diesem Jahr investieren wir den größten Teil unseres Budgets in die technische Weiterentwicklung unserer Projekte.
+			Damit stellen wir sicher, dass wir von der technischen Entwicklung nicht überrollt werden, sondern sie mitgestalten.
 		</p>
 
 		<span class="wmde-banner-mobile-only">
@@ -13,9 +16,10 @@
 		</span>
 
 		<p>
-			Das alles ist auch in Zukunft mit hohen Kosten verbunden. Da Wikipedia als gebühren- und werbefreie Wissensplattform kein Geld verdient, sind wir auf verlässliche
-			Unterstützung durch engagierte Fördermitglieder angewiesen. Es sind allerdings, trotz der Beliebtheit von Wikipedia, immer noch recht wenige Menschen, die dies
-			leisten.
+			Das alles ist auch in Zukunft mit hohen Kosten verbunden.
+			Da Wikipedia als gebühren- und werbefreie Wissensplattform kein Geld verdient,
+			sind wir auf verlässliche Unterstützung durch engagierte Fördermitglieder angewiesen.
+			Es sind allerdings, trotz der Beliebtheit von Wikipedia, immer noch recht wenige Menschen, die dies leisten.
 		</p>
 
 		<span class="wmde-banner-mobile-only">
@@ -23,8 +27,10 @@
 		</span>
 
 		<p>
-			Daher unsere Bitte: Werden Sie Fördermitglied und helfen Sie uns, die kommenden technologischen Herausforderungen zu meistern. Damit die Freiwilligen stets unter
-			besten Bedingungen arbeiten können – und Sie sich auch in Zukunft auf Wikipedia als Ort des freien Zugangs zu Wissen und Bildung verlassen können.
+			Daher unsere Bitte: <span class="highlight">Werden Sie Fördermitglied und helfen Sie uns,
+			die kommenden technologischen Herausforderungen zu meistern.</span> Damit die Freiwilligen stets unter
+			besten Bedingungen arbeiten können – und Sie sich auch in Zukunft auf Wikipedia als Ort des freien Zugangs
+			zu Wissen und Bildung verlassen können.
 		</p>
 
 		<p>
