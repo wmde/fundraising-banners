63cc622e2e010ef68f78594077189cf3158e3138
Gabriel Birke <gabriel.birke@wikimedia.de>
2025-02-04 18:20:44 +0100

    Remove deprecated vitest option
    The option `environmentMatchGlobs` for defining which tests should run
    in which environment is deprecated. Vitest recommends using workspaces
    instead.
    
    Ticket: https://phabricator.wikimedia.org/T385555

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -7,11 +7,35 @@ export default defineConfig( {
 
 	test: {
 		globals: false,
-		environmentMatchGlobs: [
-			[ 'test/unit/**', 'node' ],
-			[ 'test/integration/**', 'jsdom' ],
-			[ 'test/components/**', 'jsdom' ],
-			[ 'test/banners/**', 'jsdom' ]
+		workspace: [
+			// The `just-the-defaults` directory is a "workspace" that does not exist.
+			// We only use it to define the shared defaults (coverage and resolve aliases) for the others
+			// See https://vitest.dev/guide/workspace.html
+			'just-the-defaults/*',
+			{
+				extends: true,
+				test: {
+					include: [ 'test/unit/**/*.spec.ts' ],
+					name: 'unit',
+					environment: 'node',
+				},
+			},
+			{
+				extends: true,
+				test: {
+					include: [ 'test/components/**/*.spec.ts', 'test/integration/**/*.spec.ts' ],
+					name: 'integration',
+					environment: 'jsdom',
+				},
+			},
+			{
+				extends: true,
+				test: {
+					include: [ 'test/banners/**/*.spec.ts' ],
+					name: 'banners',
+					environment: 'jsdom',
+				},
+			},
 		],
 		coverage: {
 			exclude: [
@@ -20,14 +44,14 @@ export default defineConfig( {
 				// Ignore entry points, form items, event mappings and translations in each banner.
 				// The entry points are just factories, the other files are configurations written in TypeScript
 				'banners/*/*/{banner_ctrl,banner_var,form_items,form_items_var,event_map,event_map_var,messages,messages_var}.ts',
-				
+
 				// Ignore translation files and Locale Factories
 				'src/**/messages/*',
 				'src/utils/LocaleFactory/*',
-				
+
 				// Ignore environment-specific setup files
 				'src/environment/*',
-				
+
 				// Additional non-banner files that are not relevant for coverage
 				'dashboard/*',
 				'dist/*',
@@ -38,15 +62,15 @@ export default defineConfig( {
 				'webpack.common.js',
 				'webpack.production.js',
 				'webpack.config.js',
-			]
-		}
+			],
+		},
 	},
 	resolve: {
 		alias: {
 			'@banners': path.resolve( __dirname, './banners' ),
 			'@src': path.resolve( __dirname, './src' ),
 			'@environment': path.resolve( __dirname, './src/environment/prod' ),
-			'@test': path.resolve( __dirname, './test' )
-		}
-	}
+			'@test': path.resolve( __dirname, './test' ),
+		},
+	},
 } );
