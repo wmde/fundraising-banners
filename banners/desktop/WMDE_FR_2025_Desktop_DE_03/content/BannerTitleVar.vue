311826c4cad5fb86c9c9937dc07f185fa2120b2e
Gabriel Birke <gabriel.birke@wikimedia.de>
2025-08-07 12:32:08 +0200

    Prepare campaign for WMDE_FR_2025_Desktop_DE_03
    Copy files from WMDE_FR_2025_Desktop_DE_02
    Adapt campaign_info.toml
    
    Ticket: https://phabricator.wikimedia.org/T401369

---
+++ banners/desktop/WMDE_FR_2025_Desktop_DE_03/content/BannerTitleVar.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="wmde-banner-message-header">
+		<InfoIconItalic/> <h2>Das Internet, das uns versprochen wurde</h2>
+		<p><strong>{{ liveDateAndTime.currentDate }}, {{ liveDateAndTime.currentTime }} - An alle, die Wikipedia in Deutschland nutzen</strong></p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useLiveDateAndTime } from '@src/components/composables/useLiveDateAndTime';
+import { inject, onMounted, onUnmounted } from 'vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import InfoIconItalic from '@src/components/Icons/InfoIconItalic.vue';
+
+const { getCurrentDateAndTime } = inject<DynamicContent>( 'dynamicCampaignText' );
+const { liveDateAndTime, startTimer, stopTimer } = useLiveDateAndTime( getCurrentDateAndTime );
+onMounted( startTimer );
+onUnmounted( stopTimer );
+
+</script>
