93bbb02afc52030626aa2734fd93b79b0cac46f8
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-08-21 11:41:45 +0200

    Implement CTRL for WMDE_FR_2025_Desktop_DE_04
    - The banners are based on the variant banner of desktop-de-03
    
    Ticket: https://phabricator.wikimedia.org/T402401

--- banners/desktop/WMDE_FR_2025_Desktop_DE_04/banner_var.ts
+++ banners/desktop/WMDE_FR_2025_Desktop_DE_04/banner_var.ts
@@ -1,9 +1,9 @@
 import { createVueApp } from '@src/createVueApp';
 
-import './styles/styles_var.scss';
+import './styles/styles.scss';
 
 import BannerConductor from '@src/components/BannerConductor/FallbackBannerConductor.vue';
-import Banner from './components/BannerVar.vue';
+import Banner from './components/BannerCtrl.vue';
 import FallbackBanner from './components/FallbackBanner.vue';
 import { UrlRuntimeEnvironment } from '@src/utils/RuntimeEnvironment';
 import { WindowResizeHandler } from '@src/utils/ResizeHandler';
@@ -19,7 +19,7 @@ import { LegacyTrackerWPORG } from '@src/tracking/LegacyTrackerWPORG';
 import eventMappings from './event_map';
 import { createFallbackDonationURL } from '@src/createFallbackDonationURL';
 import { LocalStorageCloseTracker } from '@src/utils/LocalCloseTracker';
-import messages from './messages_var';
+import messages from './messages';
 import { LocaleFactoryDe } from '@src/utils/LocaleFactory/LocaleFactoryDe';
 import { createFormItems } from './form_items';
 import { createFormActions } from '@src/createFormActions';
