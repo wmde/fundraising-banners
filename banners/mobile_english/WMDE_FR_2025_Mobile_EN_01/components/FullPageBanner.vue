ac7338bc045bda6bf6b3205b494f46fd0ad46d86
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-10-01 12:12:09 +0200

    Prepare campaign for C25_WMDE_Mobile_EN_01
    Ticket: https://phabricator.wikimedia.org/T405916

---
+++ banners/mobile_english/WMDE_FR_2025_Mobile_EN_01/components/FullPageBanner.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wmde-banner-full">
+		<button class="wmde-banner-full-close t-close-full-banner" @click.prevent="$emit( 'close' )">
+			<CloseIconChunky/>
+		</button>
+
+		<div class="wmde-banner-full-content">
+
+			<header class="wmde-banner-headline">
+				Is Wikipedia worth €10 to you?
+			</header>
+
+			<div class="wmde-banner-full-info">
+				<slot name="banner-text"/>
+
+				<div>
+					<slot name="progress"/>
+				</div>
+			</div>
+
+			<div class="wmde-banner-full-call-to-action"></div>
+
+			<slot name="donation-form"/>
+
+			<slot name="footer"/>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import CloseIconChunky from '@src/components/Icons/CloseIconChunky.vue';
+
+defineEmits( [ 'close' ] );
+
+</script>
