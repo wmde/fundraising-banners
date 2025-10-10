8ef4746ae8a7da71baf5f778aed8ff75728c582f
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-10-10 11:23:02 +0200

    Prepare VAR for WMDE_FR_2025_Desktop_DE_07
    Ticket: https://phabricator.wikimedia.org/T406901

---
+++ banners/desktop/WMDE_FR_2025_Desktop_DE_07/content/BannerTextVar.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="wmde-banner-message">
+		<div>
+			<p>
+				Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am heutigen
+				{{ currentDayName }}, den {{ currentDate }}, bitten wir Sie, die Unabhängigkeit von Wikipedia zu unterstützen.
+				<AnimatedText content="Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf."/>
+				Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
+				Die durchschnittliche Spende beträgt {{ averageDonation }}, doch bereits 5&nbsp;€ helfen uns weiter.
+				Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
+				Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück.
+				Vielen Dank!
+			</p>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
+import AnimatedText from '@src/components/AnimatedText/AnimatedText.vue';
+
+const {
+	currentDayName,
+	currentDate,
+	averageDonation
+} = inject<DynamicContent>( 'dynamicCampaignText' );
+
+</script>
