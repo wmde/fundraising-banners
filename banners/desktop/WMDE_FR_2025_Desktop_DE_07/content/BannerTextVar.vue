52f906696825fc24a82e68ec6f72d3e8cfaf7480
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-10-10 12:29:20 +0200

    Implement VAR for WMDE_FR_2025_Desktop_DE_07
    Ticket: https://phabricator.wikimedia.org/T406901

--- banners/desktop/WMDE_FR_2025_Desktop_DE_07/content/BannerTextVar.vue
+++ banners/desktop/WMDE_FR_2025_Desktop_DE_07/content/BannerTextVar.vue
@@ -3,13 +3,12 @@
 		<div>
 			<p>
 				Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am heutigen
-				{{ currentDayName }}, den {{ currentDate }}, bitten wir Sie, die Unabhängigkeit von Wikipedia zu unterstützen.
-				<AnimatedText content="Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf."/>
-				Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
-				Die durchschnittliche Spende beträgt {{ averageDonation }}, doch bereits 5&nbsp;€ helfen uns weiter.
-				Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
-				Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück.
-				Vielen Dank!
+				{{ currentDayName }}, den {{ currentDate }}, bitten wir Sie die Unabhängigkeit von Wikipedia zu
+				unterstützen. <AnimatedText content="Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf."/> Die meisten Menschen spenden,
+				weil sie Wikipedia nützlich finden. Die durchschnittliche Spende beträgt 22,89&nbsp;€, doch bereits
+				5&nbsp;€ helfen uns weiter. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee
+				geschenkt? Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück.
+				Jeder Beitrag hilft – am wirksamsten ist dabei eine jährliche Spende von z.B. 5&nbsp;€. Vielen Dank!
 			</p>
 		</div>
 	</div>
@@ -22,8 +21,7 @@ import AnimatedText from '@src/components/AnimatedText/AnimatedText.vue';
 
 const {
 	currentDayName,
-	currentDate,
-	averageDonation
+	currentDate
 } = inject<DynamicContent>( 'dynamicCampaignText' );
 
 </script>
