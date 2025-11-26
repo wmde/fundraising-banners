ad278a17846ad72a578a2eae5a43f46895c5b86c
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-11-26 11:56:20 +0100

    Prepare CTRL for WMDE_FR_2025_Mobile_EN_02
    - The banners are based on the variant banner of mobile-en-01
    
    Ticket: https://phabricator.wikimedia.org/T410987

--- banners/mobile_english/WMDE_FR_2025_Mobile_EN_02/content/BannerText.vue
+++ banners/mobile_english/WMDE_FR_2025_Mobile_EN_02/content/BannerText.vue
@@ -2,19 +2,14 @@
 	<div class="wmde-banner-message">
 		<div>
 			<p class="banner-text-title">
-				&#8220;Wikipedia is not for sale.&#8221; – A personal appeal from Wikipedia-Founder Jimmy Wales
+				Wikipedia is not for sale
 			</p>
 			<p>
-				Please don't ignore this 1-minute read. This {{ currentDayName }}, {{ liveDateAndTime.currentDate }},
-				at {{ liveDateAndTime.currentTime }} I ask you to
-				reflect on the number of times you visited Wikipedia in the past year, the value you got from it,
-				and whether you're able to give €10 back. If you can, please join the 1% of readers who give. <span
-					class="wmde-banner-text-animated-highlight">If everyone reading this right now gave just €10, we'd
-					hit our goal in a couple of hours.</span> It's hard to know what
-				to trust online these days. Disinformation and scammers are everywhere. We are passionate about our
-				model because we want everyone to have equal access to quality information - something that is becoming
-				harder and harder to find online. If Wikipedia has given you €10 worth of knowledge this year, please
-				give back. Thank you.
+				We may be coming at an inopportune moment, but please don't click away! On this {{ currentDayName }}, at
+				{{ currentDate }}, we're asking you to support Wikipedia's independence. {{ campaignDaySentence }}
+				<AnimatedText :content="visitorsVsDonorsSentence"/> Most people donate because they find Wikipedia
+				useful. The average donation is {{ averageDonation }}, but even €5 helps us. Has Wikipedia given you the value of a cup
+				of coffee this year? Then choose to be one of the rare exceptions and give something back. Thank you!
 			</p>
 		</div>
 	</div>
@@ -24,6 +19,7 @@
 import { inject, watch } from 'vue';
 import { DynamicContent } from '@src/utils/DynamicContent/DynamicContent';
 import { useLiveDateAndTime } from '@src/components/composables/useLiveDateAndTime';
+import AnimatedText from '@src/components/AnimatedText/AnimatedText.vue';
 
 interface Props {
 	playLiveText: boolean
@@ -33,10 +29,14 @@ const props = defineProps<Props>();
 
 const {
 	currentDayName,
-	getCurrentDateAndTime
+	getCurrentDateAndTime,
+	visitorsVsDonorsSentence,
+	campaignDaySentence,
+	currentDate,
+	averageDonation
 }: DynamicContent = inject( 'dynamicCampaignText' );
 
-const { liveDateAndTime, startTimer, stopTimer } = useLiveDateAndTime( getCurrentDateAndTime );
+const { startTimer, stopTimer } = useLiveDateAndTime( getCurrentDateAndTime );
 
 watch( () => props.playLiveText, ( shouldPlay: boolean ) => {
 	if ( shouldPlay ) {
