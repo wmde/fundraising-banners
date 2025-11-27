f2f78ee4b994a72fe03a4625cfecbbfe9e9b289d
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-11-27 22:02:29 +0100

    Implement VAR for WMDE_FR_2025_Desktop_EN_05
    - Submitting the banner form redirects users to the variant donation form (ap=2)
    - Update content
    
    Ticket: https://phabricator.wikimedia.org/T411003

--- banners/english/WMDE_FR_2025_Desktop_EN_05/banner_var.ts
+++ banners/english/WMDE_FR_2025_Desktop_EN_05/banner_var.ts
@@ -3,7 +3,7 @@ import { createVueApp } from '@src/createVueApp';
 import './styles/styles.scss';
 
 import BannerConductor from '@src/components/BannerConductor/FallbackBannerConductor.vue';
-import Banner from './components/BannerCtrl.vue';
+import Banner from './components/BannerVar.vue';
 import FallbackBanner from './components/FallbackBanner.vue';
 import { UrlRuntimeEnvironment } from '@src/utils/RuntimeEnvironment';
 import { WindowResizeHandler } from '@src/utils/ResizeHandler';
@@ -68,7 +68,7 @@ const currencyFormatter = localeFactory.getCurrencyFormatter();
 
 app.provide( 'currencyFormatter', currencyFormatter );
 app.provide( 'formItems', createFormItems( translator, currencyFormatter.euroAmount.bind( currencyFormatter ) ) );
-app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount, { locale: Locales.EN, ap: '0' } ) );
+app.provide( 'formActions', createFormActions( page.getTracking(), impressionCount, { locale: Locales.EN, ap: '2' } ) );
 app.provide( 'tracker', tracker );
 app.provide( 'timer', new WindowTimer() );
 app.provide( 'currentCampaignTimePercentage', currentCampaignTimePercentage( new Date(), page.getCampaignParameters() ) );
