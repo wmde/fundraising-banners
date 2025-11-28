1f2ddd732ccd89aa107afd760929972e769298d2
Abban Dunne <abban.dunne@wikimedia.de>
2025-11-28 16:04:52 +0100

    Add new mobile banner
    This is an experimental banner with new functionality,
    styles, and accessibility.
    
    Ticket: https://phabricator.wikimedia.org/T409802

---
+++ banners/mobile/WMDE_FR_flash_banner_12/content/BannerSlides.vue
@@ -0,0 +1,26 @@
+<template>
+	<KeenSliderSlide>
+		<p><strong>Warum Wikipedia wichtig ist</strong></p>
+		<p>Wikipedia ist eines der meistbesuchten Websites der Welt und komplett werbefrei. <AnimatedText :is-visible="currentSlide === 0">Deine ehrliche Einschätzung hilft uns, dir die passende Spendenhöhe vorzuschlagen.</AnimatedText> Deine ehrliche Einschätzung hilft uns lorem ipusm dol</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, autem natus optio quo quod repudiandae? Consequatur consequuntur cumque ex omnis, provident ullam vitae. Adipisci et hic maiores voluptate. Deserunt, voluptatibus?</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, autem natus optio quo quod repudiandae? Consequatur consequuntur cumque ex omnis, provident ullam vitae. Adipisci et hic maiores voluptate. Deserunt, voluptatibus?</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, autem natus optio quo quod repudiandae? Consequatur consequuntur cumque ex omnis, provident ullam vitae. Adipisci et hic maiores voluptate. Deserunt, voluptatibus?</p>
+	</KeenSliderSlide>
+</template>
+
+<script setup lang="ts">
+import KeenSliderSlide from '../components/KeenSliderSlide.vue';
+import AnimatedText from '../components/AnimatedText.vue';
+
+interface Props {
+	currentSlide: number
+}
+
+defineProps<Props>();
+</script>
