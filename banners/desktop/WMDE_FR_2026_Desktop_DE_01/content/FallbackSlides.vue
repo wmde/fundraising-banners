9ac17d73bb6635c7cbfad19caa54bde68fa49ad7
Sperling-0 <tanuja.doriya@wikimedia.de>
2026-06-25 17:39:10 +0200

    Prepare campaign for WMDE_FR_2026_Desktop_DE_01
    Ticket: https://phabricator.wikimedia.org/T429779

---
+++ banners/desktop/WMDE_FR_2026_Desktop_DE_01/content/FallbackSlides.vue
@@ -0,0 +1,27 @@
+<template>
+	<KeenSliderSlide :is-current="currentSlide === 0">
+		<p>
+			Hat Wikipedia Ihnen Wissen im Wert einer Tasse Kaffee geschenkt?
+		</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide :is-current="currentSlide === 1">
+		<p>
+			Dann entscheiden Sie sich, etwas zurückzugeben. Danke!
+		</p>
+	</KeenSliderSlide>
+	<KeenSliderSlide :is-current="currentSlide === 2">
+		<FallbackBank/>
+	</KeenSliderSlide>
+</template>
+
+<script setup lang="ts">
+import KeenSliderSlide from '@src/components/Slider/KeenSliderSlide.vue';
+import FallbackBank from '@src/components/FallbackBanner/FallbackBank.vue';
+
+interface Props {
+	currentSlide: number
+}
+
+defineProps<Props>();
+
+</script>
