6b8d6ba2d5a3e52020a4abdb9d0729fc37586249
Sperling-0 <tanuja.doriya@wikimedia.de>
2026-06-30 21:17:38 +0200

    Fix slider count
    - Update content
    
    Ticket: https://phabricator.wikimedia.org/T429787

--- banners/mobile/WMDE_FR_2026_Mobile_DE_01/content/BannerSlides.vue
+++ banners/mobile/WMDE_FR_2026_Mobile_DE_01/content/BannerSlides.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<KeenSliderSlide :is-current="currentSlide === 1">
+	<KeenSliderSlide :is-current="currentSlide === 0">
 		<p><strong>Hi,</strong></p>
 		<p>
 			vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am heutigen
@@ -9,21 +9,21 @@
 		</p>
 	</KeenSliderSlide>
 
-	<KeenSliderSlide :is-current="currentSlide === 2">
+	<KeenSliderSlide :is-current="currentSlide === 1">
 		<p>
 			<AnimatedText content="Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf."/>
 			Die meisten spenden, weil sie Wikipedia nützlich finden.
 		</p>
 	</KeenSliderSlide>
 
-	<KeenSliderSlide :is-current="currentSlide === 3">
+	<KeenSliderSlide :is-current="currentSlide === 2">
 		<p>
 			Die durchschnittliche Spende beträgt {{ averageDonation }}, doch bereits 10&nbsp;€ helfen uns weiter. Hat
 			Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
 		</p>
 	</KeenSliderSlide>
 
-	<KeenSliderSlide :is-current="currentSlide === 4">
+	<KeenSliderSlide :is-current="currentSlide === 3">
 		<p>
 			Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu sein, und geben Sie etwas zurück.
 			<strong> Vielen Dank!</strong>
