19be2525627c2962caba67aca55723fadd62eb27
Abban Dunne <abban.dunne@wikimedia.de>
2026-07-16 12:33:12 +0200

    Log event when banners are shown
    This adjusts the banner shown event to log when
    a main banner is shown.
    
    Bug: T432328

--- src/tracking/LegacyEventTracking/mapShownEvent.ts
+++ src/tracking/LegacyEventTracking/mapShownEvent.ts
@@ -5,9 +5,8 @@ import { createViewportInfo } from '@src/tracking/LegacyEventTracking/createView
 
 export function mapShownEvent( shownEvent: ShownEvent ): WMDESizeIssueEvent|WMDELegacyBannerEvent {
 	if ( shownEvent.feature === 'FallbackBanner' ) {
-		return new WMDESizeIssueEvent( `fallback-banner-shown`, createViewportInfo(), 1 );
+		return new WMDESizeIssueEvent( 'fallback-banner-shown', createViewportInfo(), 1 );
 	}
 
-	// We don't track other "shown" events, hence the trackingRate of 0
-	return new WMDELegacyBannerEvent( 'untracked-shown-event', 0 );
+	return new WMDELegacyBannerEvent( 'banner-shown', 1 );
 }
