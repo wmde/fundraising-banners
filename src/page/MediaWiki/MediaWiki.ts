5f6a2df8c5e1a0bdded0b392c5e7027bf1228081
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2026-09-10 17:13:36 +0200

    DNM POC: Add popup when banner is closed
    This adds: a method to create an OOUI PopupWidget to the MediaWiki
    interface; a method to show the tooltip on the donate link to the Page
    interface; and a call to the latter to the ClosedState.
    
    The code in showDonateLinkTooltip() is largely based on [1], though I
    took the liberty of rewriting the href update in plain JS. (We still
    need jQuery for the interactions with OOUI, though.) It might be prudent
    to check the licensing situation of that code (is it *only* CC BY-SA 4.0
    by virtue of being on Meta-Wiki? or can we find a more convenient
    license for it? if not, how do we ensure compliance with CC BY-SA 4.0?).
    
    For this proof of concept, this is just added to all banners straight in
    ClosedState – we might want it to be configurable or only happen on
    certain var banners, though. The tests can also surely be improved (I
    just added the bare minimum to make TypeScript happy about the added
    methods).
    
    [1]: https://meta.wikimedia.org/w/index.php?title=MediaWiki:FundraisingBanners/CoreJS-2025.js&oldid=30935269
    
    Ticket: https://phabricator.wikimedia.org/T436929

--- src/page/MediaWiki/MediaWiki.ts
+++ src/page/MediaWiki/MediaWiki.ts
@@ -3,6 +3,22 @@ import type { SizeIssue } from '@src/page/MediaWiki/SizeIssue';
 import type { BannerEvent } from '@src/page/MediaWiki/BannerEvent';
 import type { BannerCategory } from '@src/components/BannerConductor/BannerCategory';
 
+export interface PopupWidgetConfig {
+	$content?: JQuery;
+	padded?: boolean;
+	autoClose?: boolean;
+	align?: 'forwards' | 'backwards' | 'center' | 'force-left' | 'force-right';
+	autoFlip?: boolean;
+	$floatableContainer?: JQuery;
+	position?: 'above' | 'below' | 'before' | 'after';
+}
+
+/** https://doc.wikimedia.org/oojs-ui/master/js/OO.ui.PopupWidget.html */
+export interface PopupWidgetInstance {
+	$element: JQuery;
+	toggle: ( show?: boolean ) => this;
+}
+
 export interface MediaWiki {
 	getConfigItem( name: string ): any;
 	isShowingContentPage: () => boolean;
@@ -14,4 +30,5 @@ export interface MediaWiki {
 	preventBannerDisplayUntilEndOfCampaign: ( bannerCategory: BannerCategory ) => void;
 	preventBannerDisplayForHours: ( hours: number, bannerCategory: BannerCategory ) => void;
 	setBannerLoadedButHidden: () => void;
+	newPopupWidget: ( config: PopupWidgetConfig ) => Promise<PopupWidgetInstance>;
 }
