0d176c4591534e1742747056d9dab65f66810ee4
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2026-09-11 13:05:51 +0200

    Fix ESLint and TypeScript
    I thought TypeScript was already working since webpack had stopped
    complaining about it, but it was still failing in CI. This works, I
    think.
    
    It’s pretty hacky. I couldn’t get any version of `import` to works to
    make the JQuery type available in MediaWiki.ts, so eventually I resorted
    to the /// <reference /> syntax, which works, and disabled the ESLint
    rule that wants to forbid it. ESLint itself doesn’t know about $, of
    course, so suppress some no-undef errors for that; we could also declare
    jQuery globals in the config, but I think it’s actually good that jQuery
    usage is limited in this way: I don’t think we want to start using it
    elsewhere in the banners, so it’s desirable that ESLint warns about it.

--- src/page/MediaWiki/MediaWiki.ts
+++ src/page/MediaWiki/MediaWiki.ts
@@ -1,20 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../../../node_modules/@types/jquery/JQuery.d.ts" />
 import type { LegacyBannerEvent } from '@src/page/MediaWiki/LegacyBannerEvent';
 import type { SizeIssue } from '@src/page/MediaWiki/SizeIssue';
 import type { BannerEvent } from '@src/page/MediaWiki/BannerEvent';
 import type { BannerCategory } from '@src/components/BannerConductor/BannerCategory';
 
 export interface PopupWidgetConfig {
+	/* eslint-disable no-undef */
 	$content?: JQuery;
+	$floatableContainer?: JQuery;
+	/* eslint-enable no-undef */
 	padded?: boolean;
 	autoClose?: boolean;
 	align?: 'forwards' | 'backwards' | 'center' | 'force-left' | 'force-right';
 	autoFlip?: boolean;
-	$floatableContainer?: JQuery;
 	position?: 'above' | 'below' | 'before' | 'after';
 }
 
 /** https://doc.wikimedia.org/oojs-ui/master/js/OO.ui.PopupWidget.html */
 export interface PopupWidgetInstance {
+	// eslint-disable-next-line no-undef
 	$element: JQuery;
 	toggle: ( show?: boolean ) => this;
 }
